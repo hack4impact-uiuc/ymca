8da27aedb57695cbf2be14d80d0898b59273e1ec
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-10-01 14:42:46 -0500

    Remove jade rendering in api, add backend linting

--- api/app.js
+++ api/app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var testAPIRouter = require("./routes/testAPI");
 
@@ -22,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
 
