69c29b45ccaa4efd7e3000bc318b3af188034e50
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-10-01 03:25:46 -0500

    Set up express api and connect to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,47 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: '' };
+  }
+
+  UNSAFE_componentWillMount() {
+    this.callAPI();
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  render() {
+    const { apiResponse } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>{apiResponse}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
