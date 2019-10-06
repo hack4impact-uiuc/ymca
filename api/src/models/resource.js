bc3f2ea449741a1e52debadada1ed022ba215fc3
ashwin1104 <ashwin.saxena1@gmail.com>
2019-10-06 17:06:38 -0500

    Changed otherContacts field to contact, and the OtherContact schema name to Contact.

--- api/src/models/resource.js
+++ api/src/models/resource.js
@@ -5,8 +5,8 @@ const PhoneNumber = new mongoose.Schema({
   phoneNumber: { type: String, required: true },
 });
 
-const OtherContact = new mongoose.Schema({
-  roleOfContact: { type: String, required: true },
+const Contact = new mongoose.Schema({
+  role: { type: String, required: true },
   name: { type: String, required: true },
   email: { type: String, required: false },
   phoneNumber: { type: String, required: false },
@@ -26,7 +26,7 @@ const Resource = new mongoose.Schema({
   website: { type: String, required: false },
   email: { type: String, required: false },
   phoneNumbers: { type: [PhoneNumber], required: false },
-  otherContacts: { type: [OtherContact], required: false },
+  contacts: { type: [Contact], required: false },
   address: { type: String, required: false },
   city: { type: String, required: false },
   hoursOfOperation: { type: String, required: false },
