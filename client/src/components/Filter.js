9f2287cd07fcf4029c573df49b8e4652fa157964
Skyler Shi <wwwskylercom@hotmail.com>
2019-10-08 21:02:06 -0500

    add several filters for filter page

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -7,12 +7,12 @@ import {
 } from 'reactstrap';
 import '../App.css';
 import '../css/Filter.css';
-import FilterPreview from './FilterPreview.js'
+import FilterPreview from './FilterPreview';
 
 export default class Filter extends Component<Props, State> {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       categoryDropdownOpen: false,
       subcategoryDropdownOpen: false,
@@ -23,112 +23,137 @@ export default class Filter extends Component<Props, State> {
       languageSelected: '',
 
       categoryDropdownOpenItems: {
-        'Abuse, Domestic Violence, Exploitation': ['1','2','3'],
-        'Children Park Registration': ['4','5','6'],
-        'Drug Alcohol Addiction Treatment': ['7','8','9'],
-        'Education Adult': ['10','11','12'],
-        'Employment': ['13','14','15']
+        'Abuse, Domestic Violence, Exploitation': ['1', '2', '3'],
+        'Children Park Registration': ['4', '5', '6'],
+        'Drug Alcohol Addiction Treatment': ['7', '8', '9'],
+        'Education Adult': ['10', '11', '12'],
+        Employment: ['13', '14', '15'],
       },
-      languages: ["English", "Spanish", "Chinese", "Japanese"],
+      languages: ['English', 'Spanish', 'Chinese', 'Japanese'],
 
-      resources: []
+      resources: [],
     };
   }
 
   categoryToggle = () => {
-    this.setState({
-      categoryDropdownOpen: !this.state.categoryDropdownOpen,
-    });
-  }
+    this.setState(prevState => ({
+      categoryDropdownOpen: !prevState.categoryDropdownOpen,
+    }));
+  };
+
   categorySelect = value => {
     this.setState({
       categorySelected: value,
-      subcategorySelected: ''
+      subcategorySelected: '',
     });
-  }
+  };
+
   subcategoryToggle = () => {
-    this.setState({
-      subcategoryDropdownOpen: !this.state.subcategoryDropdownOpen,
-    });
-  }
+    this.setState(prevState => ({
+      subcategoryDropdownOpen: !prevState.subcategoryDropdownOpen,
+    }));
+  };
+
   subcategorySelect = value => {
     this.setState({
       subcategorySelected: value,
 
-      resources: ['Dummy Resource A', 'Dummy Resource B', 'Dummy Resource C']
+      resources: ['Dummy Resource A', 'Dummy Resource B', 'Dummy Resource C'],
     });
-  }
+  };
 
   languageToggle = () => {
-    this.setState({
-      languageDropdownOPen: !this.state.languageDropdownOPen,
-    });
-  }
+    this.setState(prevState => ({
+      languageDropdownOPen: !prevState.languageDropdownOPen,
+    }));
+  };
+
   languageSelect = value => {
     this.setState({
-      languageSelected: value
+      languageSelected: value,
     });
-  }
-
+  };
 
   render() {
     return (
       <div className="filter-component">
         <div className="dropdowns-container">
-          <ButtonDropdown isOpen={this.state.categoryDropdownOpen} toggle={this.categoryToggle}>
-            <DropdownToggle caret>{(this.state.categorySelected=='') ? 'Resource Category': this.state.categorySelected}</DropdownToggle>
+          <ButtonDropdown
+            isOpen={this.state.categoryDropdownOpen}
+            toggle={this.categoryToggle}
+          >
+            <DropdownToggle caret>
+              {this.state.categorySelected === ''
+                ? 'Resource Category'
+                : this.state.categorySelected}
+            </DropdownToggle>
             <DropdownMenu>
-              {Object.keys(this.state.categoryDropdownOpenItems).map((value) => {
+              {Object.keys(this.state.categoryDropdownOpenItems).map(value => {
                 return (
-                  <DropdownItem >
+                  <DropdownItem>
                     <div onClick={() => this.categorySelect(value)}>
                       {value}
                     </div>
-                  </DropdownItem>)
+                  </DropdownItem>
+                );
               })}
             </DropdownMenu>
           </ButtonDropdown>
 
-          <ButtonDropdown isOpen={this.state.subcategoryDropdownOpen} toggle={this.subcategoryToggle}>
-            <DropdownToggle caret>{(this.state.subcategorySelected=='') ? 'Resource Subcategory': this.state.subcategorySelected}</DropdownToggle>
+          <ButtonDropdown
+            isOpen={this.state.subcategoryDropdownOpen}
+            toggle={this.subcategoryToggle}
+          >
+            <DropdownToggle caret>
+              {this.state.subcategorySelected === ''
+                ? 'Resource Subcategory'
+                : this.state.subcategorySelected}
+            </DropdownToggle>
             <DropdownMenu>
-              {this.state.categorySelected != '' && (this.state.categoryDropdownOpenItems[this.state.categorySelected]).map((value) => {
-                return (
-                  <DropdownItem >
-                    <div onClick={() => this.subcategorySelect(value)}>
-                      {value}
-                    </div>
-                  </DropdownItem>)
-              })}
+              {this.state.categorySelected !== '' &&
+                this.state.categoryDropdownOpenItems[
+                  this.state.categorySelected
+                ].map(value => {
+                  return (
+                    <DropdownItem>
+                      <div onClick={() => this.subcategorySelect(value)}>
+                        {value}
+                      </div>
+                    </DropdownItem>
+                  );
+                })}
             </DropdownMenu>
           </ButtonDropdown>
 
-          <ButtonDropdown isOpen={this.state.languageDropdownOPen} toggle={this.languageToggle}>
-            <DropdownToggle caret>{(this.state.languageSelected=='') ? 'Language': this.state.languageSelected}</DropdownToggle>
+          <ButtonDropdown
+            isOpen={this.state.languageDropdownOPen}
+            toggle={this.languageToggle}
+          >
+            <DropdownToggle caret>
+              {this.state.languageSelected === ''
+                ? 'Language'
+                : this.state.languageSelected}
+            </DropdownToggle>
             <DropdownMenu>
-              {(this.state.languages).map((value) => {
+              {this.state.languages.map(value => {
                 return (
-                  <DropdownItem >
+                  <DropdownItem>
                     <div onClick={() => this.languageSelect(value)}>
                       {value}
                     </div>
-                  </DropdownItem>)
+                  </DropdownItem>
+                );
               })}
             </DropdownMenu>
           </ButtonDropdown>
         </div>
 
-        <div className='filter-preview-container'>
-          {this.state.resources.map((value) => {
-            return (
-              <FilterPreview resourceName={value}>
-
-              </FilterPreview>)
+        <div className="filter-preview-container">
+          {this.state.resources.map(value => {
+            return <FilterPreview resourceName={value} />;
           })}
         </div>
       </div>
     );
   }
 }
-
-
--- client/src/components/FilterPreview.js
+++ client/src/components/FilterPreview.js
@@ -6,26 +6,20 @@ import {
   DropdownItem,
 } from 'reactstrap';
 import '../App.css';
-import '../css/FilterPreview.css'
-
+import '../css/FilterPreview.css';
 
 export default class FilterPreview extends Component<Props, State> {
-    constructor(props) {
-      super(props);
-      this.state = {
-        dropdownOpen: false,
-      };
-    }
-  
-    render() {
-      return (
-        <div className='filter-preview'>
-            <div className='filter-preview-top'>
-            </div>
-            <div className='filter-preview-bottom'>
-                {this.props.resourceName}
-            </div>
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
+
+  render() {
+    return (
+      <div className="filter-preview">
+        <div className="filter-preview-top" />
+        <div className="filter-preview-bottom">{this.props.resourceName}</div>
+      </div>
+    );
+  }
+}
