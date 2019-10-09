3fc523fb5bf5a50d01edd5bc9b99e0f418c81713
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-10-09 16:14:22 -0500

    Change port to 9000, finalize connections

--- api/src/index.js
+++ api/src/index.js
@@ -10,7 +10,7 @@ mongoose.connection
   .once('open', () => console.log('Connected to MongoLab instance.'))
   .on('error', error => console.log('Error connecting to MongoLab:', error));
 
-const API_PORT = process.env.PORT === undefined ? 8080 : process.env.PORT;
+const API_PORT = process.env.PORT === undefined ? 9000 : process.env.PORT;
 app.listen(API_PORT, async () =>
   console.log(`YMCA API server listening on port ${API_PORT}!`),
 );
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 const testAPI = require('./testAPI');
 
-// mydomain.com/
 router.get('/', (req, res) => {
   res.send('hi');
 });
