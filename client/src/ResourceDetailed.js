99432074ba74b01925b0ba65f287c82d899e743d
anoojlal <anoojl2@illinois.edu>
2019-10-10 20:13:17 -0500

    :fire: merge

--- client/src/ResourceDetailed.js
+++ client/src/ResourceDetailed.js
@@ -1,38 +1,132 @@
 import React, { Component } from 'react';
 import './ResourceDetailed.css';
+import { Container, Row, Col } from 'reactstrap';
 
 export default class ResourceDetailed extends Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //   name: '',
-    //   phone: 0,
-    //   address: '',
-    //   description: '',
-    //   hours: [],
-    //   city: '',
-    //   county: '',
-    //   requiredDocs: [],
-    //   cost: 0,
-    //   languages: [],
-    // };
+    this.state = {
+      name: 'Resource Name',
+      phone: '0000000000',
+      email: 'email@address.com',
+      address: '123 Street Name',
+      website: "website.com",
+      description: 'This is a description of the resource. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus egestas ac neque sit amet commodo. Aenean eu ultricies enim. Etiam consectetur sem nisl, sit amet aliquet risus posuere vitae.',
+      hours: [
+        {
+            start: "09:00",
+            end: "17:00"
+        },
+        {
+            start: "09:00",
+            end: "17:00"
+        },
+        {
+            start: "09:00",
+            end: "17:00"
+        },
+        {
+            start: "09:00",
+            end: "17:00"
+        },
+        {
+            start: "09:00",
+            end: "17:00"
+        }
+      ],
+      city: 'City Name',
+      county: 'County Name',
+      requiredDocs: [
+          "Required Form 1",
+          "Required Form 2",
+          "Required Form 3",
+          "Required Form 4",
+      ],
+      cost: 1.23,
+      languages: [
+          "Language 1",
+          "Language 2"
+      ],
+    };
   }
 
   render() {
-    // const {
-    //   name,
-    //   phone,
-    //   address,
-    //   description,
-    //   hours,
-    //   city,
-    //   county,
-    //   requiredDocs,
-    //   cost,
-    //   languages,
-    // } = this.state;
+    const {
+      name,
+      phone,
+      email,
+      address,
+      website,
+      description,
+      hours,
+      city,
+      county,
+      requiredDocs,
+      cost,
+      languages,
+    } = this.state;
 
-    return <div>Resources</div>;
+    return (
+        <div className="ResourceDetailed">
+            <Container>
+                <Row>
+                    <Col className="resourceName">
+                        {name}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {description}
+                    </Col>
+                    <Col>
+                        {hours.map((day, index) => {
+                            let dayOfWeek = "";
+
+                            switch (index) {
+                                case 0:
+                                    dayOfWeek = "Monday";
+                                    break;
+                                case 1:
+                                    dayOfWeek = "Tuesday";
+                                    break;
+                                case 2:
+                                    dayOfWeek = "Wednesday";
+                                    break;
+                                case 3:
+                                    dayOfWeek = "Thursday";
+                                    break;
+                                case 4:
+                                    dayOfWeek = "Friday";
+                                    break;
+                                case 5:
+                                    dayOfWeek = "Saturday";
+                                    break;
+                                case 6:
+                                    dayOfWeek = "Sunday";
+                                    break;
+                                default:
+                                    dayOfWeek = "ERROR";
+                                    break;
+                            }
+
+                            return (dayOfWeek + ": " + day.start + " - " + day.end + "\n");
+                        })}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {"Contact\n"}
+                        {phone + "\n"}
+                        {"Email: " + email + "\n"}
+                        {"Website: " + website + "\n"}
+                    </Col>
+                    <Col>
+                        {"Map showing " + address + ", " + city + "\n"}
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
   }
 }
