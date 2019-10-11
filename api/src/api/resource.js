a441e3474500ad6a54a2cf9d4a99846ceedc8a53
Jacqueline Osborn <jackieo5023@gmail.com>
2019-10-10 21:29:36 -0500

    Fix endpoints

--- api/src/api/resource.js
+++ api/src/api/resource.js
@@ -7,39 +7,42 @@ const Resource = require('../models/resource');
 router.get(
   '/',
   errorWrap(async (req, res) => {
-    const resources = await Resource.find();
-    res.send(resources.toJSON());
+    const { category } = req.query;
+    let resources;
+    if (category != null) {
+      resources = await Resource.find({ category });
+    } else {
+      resources = await Resource.find();
+    }
+    res.json({
+      code: 200,
+      message: '',
+      success: true,
+      result: resources,
+    });
   }),
 );
 
-// Get resources by category
-router.get(
-  '/:category',
-  errorWrap(async (req, res) => {
-    const categoryName = req.params.category;
-    const resources = await Resource.find({ category: categoryName });
-    res.send(resources.toJSON());
-  }),
-);
-
-// Delete resource by ID
-router.delete(
-  '/:id',
-  errorWrap(async (req, res) => {
-    const resourceId = req.params.id;
-    try {
-      const Resource = await Resource.findByIdAndDelete(resourceId);
-      res.send({ code: 200, success: true });
-    } catch {}
-  }),
-);
 // Get a resource by id
 router.get(
   '/:id',
   errorWrap(async (req, res) => {
     const { id } = req.params;
     const resource = await Resource.findById(id);
-    res.send(resource.toJSON());
+    if (resource === null) {
+      res.status(400).json({
+        code: 400,
+        message: `Cannot find resource ${id}`,
+        success: false,
+        result: null,
+      });
+    }
+    res.json({
+      code: 200,
+      message: `Successfully found resource ${id}`,
+      success: true,
+      result: resource,
+    });
   }),
 );
 
@@ -47,9 +50,11 @@ router.get(
 router.post(
   '/',
   errorWrap(async (req, res) => {
-    const resource = Resource.build(req.body);
+    const resource = new Resource(req.body);
     const newResource = await resource.save();
-    res.status(201).send(newResource);
+    res
+      .status(201)
+      .json({ code: 201, message: '', success: true, result: newResource });
   }),
 );
 
@@ -61,7 +66,22 @@ router.put(
     const updatedResource = await Resource.findByIdAndUpdate(id, req.body, {
       new: true,
     });
-    res.send(updatedResource);
+    res.json({
+      code: 200,
+      message: '',
+      success: true,
+      result: updatedResource,
+    });
+  }),
+);
+
+// Delete resource by ID
+router.delete(
+  '/:id',
+  errorWrap(async (req, res) => {
+    const resourceId = req.params.id;
+    const Resource = await Resource.findByIdAndDelete(resourceId);
+    res.json({ code: 200, message: '', success: true, result: null });
   }),
 );
 
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const testAPI = require('./testAPI');
+const { resource } = require('../api');
 
 router.get('/', (req, res) => {
   res.send('hi');
 });
 
-router.use('/testAPI', testAPI);
+router.use('/resources', resource);
 
 module.exports = router;
