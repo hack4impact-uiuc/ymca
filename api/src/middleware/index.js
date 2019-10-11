a530520059d39d13e5044cd14d6fb6af5aff46e7
Jacqueline Osborn <jackieo5023@gmail.com>
2019-10-10 19:59:37 -0500

    Add errorwrap to middleware index

--- api/src/middleware/index.js
+++ api/src/middleware/index.js
@@ -1,5 +1,7 @@
 const errorHandler = require('./errorHandler');
+const errorWrap = require('./errorHandler');
 
 module.exports = {
   errorHandler,
+  errorWrap,
 };
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,8 @@
-const express = require('express')
-const router = express.Router()
-const {
-  resource
-} = require('./api')
+const express = require('express');
+const router = express.Router();
+const { resource } = require('./api');
 
 // sets up the routers defined in the module /src/api
-router.use('/resource', resource)
+router.use('/resource', resource);
 
-module.exports = router
+module.exports = router;
