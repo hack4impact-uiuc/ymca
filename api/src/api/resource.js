04e380df34e5b22da6db6a94f293e532e3423b9b
ashwin1104 <ashwin.saxena1@gmail.com>
2019-10-10 20:07:00 -0500

    Add endpoints

--- api/src/api/resource.js
+++ api/src/api/resource.js
@@ -2,4 +2,36 @@ const express = require('express')
 const router = express.Router()
 const { Resource } = require('../models')
 
-module.exports = router
+// Get all resources
+router.get(
+    '/',
+    errorWrap(async (req, res) => {
+        const resources = await Resource.find()
+        res.send(resources.toJSON())
+    })
+)
+
+// Get resources by category
+router.get(
+    '/:category',
+    errorWrap(async (req, res) => {
+        const categoryName = req.params.category
+        const resources = await Resource.find({ category: categoryName })
+        res.send(resources.toJSON())
+    })
+)
+
+// Delete resource by ID
+router.delete(
+    '/:id',
+    errorWrap(async (req, res) => {
+      const resourceId = req.params.id;
+      try {
+        const Resource = await Resource.findByIdAndDelete(resourceId)
+        res.send({ code: 200, success: true })
+      }
+      catch {
+
+      }
+    })
+)
