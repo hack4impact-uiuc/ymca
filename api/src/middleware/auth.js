6b078a20680faa5f465c556f0515e9783c9ef74a
kenetec <klhester3@gmail.com>
2019-10-22 19:05:12 -0500

    Update src/middleware/auth.js

--- api/src/middleware/auth.js
+++ api/src/middleware/auth.js
@@ -16,14 +16,14 @@ const auth = async (req, res, next) => {
 
   const authRes = await authReq.json();
   if (authRes.status === 200) {
-    const { role, userid } = authReq.body;
-
-    if (authReq.body.token != undefined) {
-      // update token on client side.
-      res.set({ token: authReq.body.token });
+    const { role, uid, newToken } = authReq;
+    if (newToken !== undefined) {
+      res.set({ token: newToken });
+    } else {
+      res.set({ token });
     }
 
-    next();
+    next()
   }
 
   // if not auth'd (from https://github.com/hack4impact-uiuc/h4i-recruitment/blob/eab33c223314abb367558dc6e5cfa05d90989681/frontend/src/utils/api.js)
