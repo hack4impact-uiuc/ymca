8ba453efe5d678f0d9e5a91077cc9c9005fb1822
kenetec <klhester3@gmail.com>
2019-10-22 19:00:55 -0500

    Clean unnecessary files

--- api/src/models/ymcaEmployee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const YMCAUser = new mongoose.Schema({
-  email: { type: String, required: true },
-  role: { type: String, required: true },
-  companyId: { type: Number, required: false },
-});
-
-module.exports = mongoose.model('YMCAUser', YMCAUser);
