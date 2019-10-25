e7f159f58d9e7eb9da411b2df4b63312da126ae9
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-10-24 20:36:05 -0500

    Restructure app listening on backend

--- api/src/app.js
+++ api/src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const fs = require('fs');
 const helmet = require('helmet');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes');
 
@@ -30,6 +31,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', routes);
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 app.use(function(req, res, next) {
