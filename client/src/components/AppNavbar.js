3bcc6d614f4d5a955d789970dff598805011f31a
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-10-25 04:43:07 -0500

    Convert nav bar to ant

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,51 +1,21 @@
 import React, { Component } from 'react';
-import '../css/AppNavbar.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-} from 'reactstrap';
+import { Menu } from 'antd';
+import '../css/App.css';
 
 export default class AppNavbar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  toggle = () => {
-    const { isOpen } = this.state;
-
-    this.setState({
-      isOpen,
-    });
-  };
-
   render() {
-    const { isOpen } = this.state;
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">YMCA</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/filter">Resources</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/login">Login</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <a href="/">YMCA</a>
+        </Menu.Item>
+        <Menu.Item key="resources">
+          <a href="/filter">Resources</a>
+        </Menu.Item>
+        <Menu.Item key="login">
+          <a href="/login">Login</a>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
