227523bb500bfab50cf227bb87c2e2645483198d
Neeraj Aggarwal <hello@neerajaggarwal.com>
2019-10-27 14:29:46 -0500

    🎨Use ant design for nav bar (#30)
    * Convert nav bar to ant
    
    * Remove unused css import
    
    * Style nav bar
    
    * Finalize navigation style
    
    * Reorganize lint rules

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Layout, Menu } from 'antd';
+import '../css/Navigation.css';
+
+const { Header } = Layout;
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <Header className="header">
+        <Menu mode="horizontal">
+          <Menu.Item key="home">
+            <a href="/">YMCA</a>
+          </Menu.Item>
+          <Menu.Item key="resources">
+            <a href="/filter">Resources</a>
+          </Menu.Item>
+          <Menu.Item key="login">
+            <a href="/login">Login</a>
+          </Menu.Item>
+        </Menu>
+      </Header>
+    );
+  }
+}
