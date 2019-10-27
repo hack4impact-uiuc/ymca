a24e9c39ae6dcf1f0ff050f9df3668ae9e32a0d7
Neeraj Aggarwal <hello@neerajaggarwal.com>
2019-10-27 14:34:59 -0500

    🍓Various code improvements (#28)
    - Fixes backend link for production
    - Adds documentation about now deployment
    - Adds documentation about local development (specifically url changing for backend)
    - Changes application title in production (still React App)
    - Adds favicon
    - Fixes #32

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const instance = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: 'https://ymca.now.sh',
 });
 
 export const getCategories = () => {
