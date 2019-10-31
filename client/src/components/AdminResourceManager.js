7d61315193302aea4050e9e3efd1e437ac3dc386
kenetec <klhester3@gmail.com>
2019-10-31 02:59:26 -0500

    Remove reactstrap

--- client/src/components/AdminResourceManager.js
+++ client/src/components/AdminResourceManager.js
@@ -1,17 +1,5 @@
 import React, { useState } from 'react';
 import '../css/AdminResourceManager.css';
-
-import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  FormText,
-  Button,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-} from 'reactstrap';
 import NewResourceForm from './NewResourceForm';
 
 const AdminResourceManager = () => {
