c16113b9cefab73aa07398f74ce74e32e9251ff6
kenetec <klhester3@gmail.com>
2019-10-31 02:38:31 -0500

    Adapt financial aid form to ant design

--- client/src/components/NewResourceFinancialAidForm.js
+++ client/src/components/NewResourceFinancialAidForm.js
@@ -6,16 +6,7 @@ TODO: Implement phoneType into form.
 
 import React, { useState } from 'react';
 import '../css/NewResourcePhoneNumberForm.css';
-import {
-  Form,
-  FormGroup,
-  Label,
-  Input,
-  ListGroup,
-  ListGroupItem,
-  Button,
-  Alert,
-} from 'reactstrap';
+import { Input, Form, Button, List, Descriptions } from 'antd';
 
 type FinancialAid = {|
   education: String,
@@ -23,184 +14,100 @@ type FinancialAid = {|
   deadline: String,
 |};
 
-type FormProps = {|
-  financialAidDetails: FinancialAid,
-  setFinancialAidDetails: () => void,
-  setTotalSubmitEnabled: () => void,
-|};
-
-type EntryProps = {|
-  financialAidDetails: FinancialAid,
-  setFinancialAidDetails: () => void,
-|};
-
-const FinancialAidEntry = (props: EntryProps) => {
-  const { financialAidDetails, setFinancialAidDetails } = props;
-
-  return (
-    <ListGroupItem>
-      <div className="finAidContainer">
-        <div className="finAidEducationView">
-          {financialAidDetails.education}
-        </div>
-        <div className="finAidImmigrationStatusView">
-          {financialAidDetails.immigrationStatus}
-        </div>
-        <div className="finAidDeadlineView">{financialAidDetails.deadline}</div>
-        <div className="finAidClearButton">
-          <Button
-            color="danger"
-            onClick={e => {
-              e.preventDefault();
-              setFinancialAidDetails({});
-            }}
-          >
-            Clear
-          </Button>
-        </div>
-      </div>
-    </ListGroupItem>
-  );
-};
-
-const onSubmit = args => {
-  const {
-    e,
-    submitEnabled,
-    education,
-    immigrationStatus,
-    deadline,
-    setFinancialAidDetails,
-    setEducation,
-    setImmigrationStatus,
-    setDeadline,
-    setErrorMessage,
-  } = args;
-
-  e.preventDefault();
-
-  if (submitEnabled) {
-    setFinancialAidDetails({ education, immigrationStatus, deadline });
-  }
-};
-
-const onInputFocus = (setTotalSubmitEnabled, setSubmitEnabled) => {
-  setTotalSubmitEnabled(false);
+const onInputFocus = (setSubmitEnabled, setTotalSubmitEnabled) => {
   setSubmitEnabled(true);
+  setTotalSubmitEnabled(false);
 };
 
-const onInputBlur = (
-  setTotalSubmitEnabled,
-  setSubmitEnabled,
-  setErrorMessage,
-) => {
-  setTotalSubmitEnabled(true);
+const onInputBlur = (setSubmitEnabled, setTotalSubmitEnabled) => {
   setSubmitEnabled(false);
-  setErrorMessage('');
+  setTotalSubmitEnabled(true);
 };
 
-const NewResourceFinancialAidForm = (props: FormProps) => {
+const FinancialAidForm = Form.create({ name: 'financialAid' })(props => {
   const {
     financialAidDetails,
     setFinancialAidDetails,
     setTotalSubmitEnabled,
   } = props;
 
-  const [submitEnabled, setSubmitEnabled] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const { getFieldDecorator, getFieldValue, setFieldsValue } = props.form;
 
-  const [education, setEducation] = useState('');
-  const [immigrationStatus, setImmigrationStatus] = useState('');
-  const [deadline, setDeadline] = useState('');
+  const [submitEnabled, setSubmitEnabled] = useState(false);
 
   return (
-    <>
-      {errorMessage !== '' && <Alert color="danger">{errorMessage}</Alert>}
-      <ListGroup>
-        {FinancialAidEntry({ financialAidDetails, setFinancialAidDetails })}
-      </ListGroup>
-      <Form
-        onSubmit={e =>
-          onSubmit({
-            e,
-            submitEnabled,
-            education,
-            immigrationStatus,
-            deadline,
-            setEducation,
-            setImmigrationStatus,
-            setDeadline,
-            setErrorMessage,
-            setFinancialAidDetails,
-          })
-        }
-      >
-        <FormGroup>
-          <div className="finAidContainer">
-            <div className="finAidEducationInput">
-              <Input
-                id="finAidEducationInput_"
-                type="text"
-                placeholder="Education"
-                onChange={e => setEducation(e.target.value)}
-                onFocus={e =>
-                  onInputFocus(setTotalSubmitEnabled, setSubmitEnabled)
-                }
-                onBlur={e =>
-                  onInputBlur(
-                    setTotalSubmitEnabled,
-                    setSubmitEnabled,
-                    setErrorMessage,
-                  )
-                }
-              />
-            </div>
-            <div className="finAidImmigrationStatusInput">
-              <Input
-                id="finAidImmigrationStatusInput_"
-                type="text"
-                placeholder="Immigration status"
-                onChange={e => setImmigrationStatus(e.target.value)}
-                onFocus={e =>
-                  onInputFocus(setTotalSubmitEnabled, setSubmitEnabled)
-                }
-                onBlur={e =>
-                  onInputBlur(
-                    setTotalSubmitEnabled,
-                    setSubmitEnabled,
-                    setErrorMessage,
-                  )
-                }
-              />
-            </div>
-            <div className="finAidDeadlineInput">
-              <Input
-                id="finAidDeadlineInput_"
-                type="text"
-                placeholder="Deadline"
-                onChange={e => setDeadline(e.target.value)}
-                onFocus={e =>
-                  onInputFocus(setTotalSubmitEnabled, setSubmitEnabled)
-                }
-                onBlur={e =>
-                  onInputBlur(
-                    setTotalSubmitEnabled,
-                    setSubmitEnabled,
-                    setErrorMessage,
-                  )
-                }
-              />
-            </div>
-          </div>
-        </FormGroup>
-        <Input
-          type="submit"
-          value="Add information"
-          onClick={() => setSubmitEnabled(true)}
-        />
-      </Form>
-    </>
+    <Form
+      className="financialAidForm"
+      onSubmit={e => {
+        setFinancialAidDetails({
+          education: getFieldValue('education'),
+          immigrationStatus: getFieldValue('immigrationStatus'),
+          deadline: getFieldValue('deadline'),
+        });
+      }}
+    >
+      <Form.Item>
+        {getFieldDecorator('education', {})(
+          <Input
+            placeholder="Education"
+            onFocus={e => onInputFocus(setSubmitEnabled, setTotalSubmitEnabled)}
+            onBlur={e => onInputBlur(setSubmitEnabled, setTotalSubmitEnabled)}
+          />,
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator('immigrationStatus', {})(
+          <Input
+            placeholder="Immigration Status"
+            onFocus={e => onInputFocus(setSubmitEnabled, setTotalSubmitEnabled)}
+            onBlur={e => onInputBlur(setSubmitEnabled, setTotalSubmitEnabled)}
+          />,
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator('deadline', {})(
+          <Input
+            placeholder="Deadline"
+            onFocus={e => onInputFocus(setSubmitEnabled, setTotalSubmitEnabled)}
+            onBlur={e => onInputBlur(setSubmitEnabled, setTotalSubmitEnabled)}
+          />,
+        )}
+      </Form.Item>
+      <Button type="primary" htmlType="submit" className="financialAidForm">
+        Add Financial Aid Details
+      </Button>
+    </Form>
+  );
+});
+
+const FinancialAidFormItem = (props: FormProps) => {
+  const {
+    financialAidDetails,
+    setFinancialAidDetails,
+    setTotalSubmitEnabled,
+  } = props;
+
+  return (
+    <Form.Item>
+      <Descriptions title="Financial Aid Details">
+        <Descriptions.Item label="Education">
+          {financialAidDetails.education}
+        </Descriptions.Item>
+        <Descriptions.Item label="Immigration Status">
+          {financialAidDetails.immigrationStatus}
+        </Descriptions.Item>
+        <Descriptions.Item label="Deadline">
+          {financialAidDetails.deadline}
+        </Descriptions.Item>
+      </Descriptions>
+      <FinancialAidForm
+        financialAidDetails={financialAidDetails}
+        setFinancialAidDetails={setFinancialAidDetails}
+        setTotalSubmitEnabled={setTotalSubmitEnabled}
+        wrappedComponentRef={form => (this.form = form)}
+      />
+    </Form.Item>
   );
 };
 
-export default NewResourceFinancialAidForm;
+export default FinancialAidFormItem;
