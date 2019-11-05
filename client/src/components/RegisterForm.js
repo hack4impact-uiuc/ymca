b08c178933edfc8ccd23099c636939206bf13b4a
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2019-11-04 19:37:48 -0600

    Change login redirect flow

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Button, Form, Input } from 'antd';
 import 'antd/dist/antd.css';
 import '../css/RegisterForm.css';
@@ -10,7 +10,8 @@ class RegisterForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isAuthSuccessful: false,
+      authSuccess: false,
+      confirmDirty: true,
     };
   }
 
@@ -39,7 +40,7 @@ class RegisterForm extends Component {
         const { email, password } = values;
         register({ email, password }).then(res => {
           if (res.status === 200) {
-            this.setState({ isAuthSuccessful: true });
+            this.setState({ authSuccess: true });
           } else {
             // show error message
           }
@@ -49,7 +50,8 @@ class RegisterForm extends Component {
   };
 
   render() {
-    const { isAuthSuccessful } = this.state;
+    if (this.state.authSucess) return <Redirect to="/admin" />;
+
     const { getFieldDecorator } = this.props.form;
 
     const formItemLayout = {
@@ -77,63 +79,55 @@ class RegisterForm extends Component {
     };
 
     return (
-      <>
-        {isAuthSuccessful && <ToHomePage />}
-        <Form {...formItemLayout} onSubmit={this.onRegisterSubmit}>
-          <Form.Item label="E-mail">
-            {getFieldDecorator('email', {
-              rules: [
-                {
-                  type: 'email',
-                  message: 'The input is not valid E-mail!',
-                },
-                {
-                  required: true,
-                  message: 'Please input your E-mail!',
-                },
-              ],
-            })(<Input />)}
-          </Form.Item>
-          <Form.Item label="Create Password" hasFeedback>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please create a password!',
-                },
-                {
-                  validator: this.validateToNextPassword,
-                },
-              ],
-            })(<Input.Password />)}
-          </Form.Item>
-          <Form.Item label="Confirm Password" hasFeedback>
-            {getFieldDecorator('confirm', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Please confirm your password!',
-                },
-                {
-                  validator: this.compareToFirstPassword,
-                },
-              ],
-            })(<Input.Password onBlur={this.handleConfirmBlur} />)}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button className="reg-button" type="primary" htmlType="submit">
-              Register
-            </Button>
-          </Form.Item>
-        </Form>
-      </>
+      <Form {...formItemLayout} onSubmit={this.onRegisterSubmit}>
+        <Form.Item label="E-mail">
+          {getFieldDecorator('email', {
+            rules: [
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
+              },
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ],
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Create Password" hasFeedback>
+          {getFieldDecorator('password', {
+            rules: [
+              {
+                required: true,
+                message: 'Please create a password!',
+              },
+              {
+                validator: this.validateToNextPassword,
+              },
+            ],
+          })(<Input.Password />)}
+        </Form.Item>
+        <Form.Item label="Confirm Password" hasFeedback>
+          {getFieldDecorator('confirm', {
+            rules: [
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              {
+                validator: this.compareToFirstPassword,
+              },
+            ],
+          })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button className="reg-button" type="primary" htmlType="submit">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
-}
-
-function ToHomePage() {
-  useHistory().push('/');
-  return null;
 }
 
 RegisterForm.propTypes = {
