54b84b40fd87016e0b4be7bd925b6db520012ab1
kenetec <klhester3@gmail.com>
2019-11-07 17:35:59 -0600

    Converted App to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import AdminResourceManager from './components/AdminResourceManager';
@@ -11,24 +11,24 @@ import ResourceDetail from './components/ResourceDetail';
 import Resources from './components/Resources';
 import ResourceUnknown from './components/ResourceUnknown';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Navigation />
-        <Router>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/admin" exact component={AdminResourceManager} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-            <Route path="/resources" exact component={Resources} />
-            <Route path="/resources/unknown" component={ResourceUnknown} />
-            <Route path="/resources/:id" component={ResourceDetail} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/admin" exact component={AdminResourceManager} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/resources" exact component={Resources} />
+          <Route path="/resources/unknown" component={ResourceUnknown} />
+          <Route path="/resources/:id" component={ResourceDetail} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default App;
