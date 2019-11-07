b69bd0b5aaf8a92aebbfba91fa047c86a8df3d0a
kenetec <klhester3@gmail.com>
2019-11-07 17:10:53 -0600

    Transform Navigation to functional component

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
+// @flow
+
+import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import '../css/Navigation.css';
 
 const { Header } = Layout;
 
-export default class Navigation extends Component {
-  render() {
-    return (
-      <Header className="header">
-        <div className="logo" />
-        <Menu mode="horizontal">
-          <Menu.Item key="home">
-            <a href="/">YMCA</a>
-          </Menu.Item>
-          <Menu.Item key="resources">
-            <a href="/resources?category=All Resources">Resources</a>
-          </Menu.Item>
-          <Menu.Item key="login">
-            <a href="/login">Login</a>
-          </Menu.Item>
-        </Menu>
-      </Header>
-    );
-  }
-}
+type Props = {};
+
+const Navigation = (props: Props) => {
+  return (
+    <Header className="header">
+      <div className="logo" />
+      <Menu mode="horizontal">
+        <Menu.Item key="home">
+          <a href="/">YMCA</a>
+        </Menu.Item>
+        <Menu.Item key="resources">
+          <a href="/resources?category=All Resources">Resources</a>
+        </Menu.Item>
+        <Menu.Item key="login">
+          <a href="/login">Login</a>
+        </Menu.Item>
+      </Menu>
+    </Header>
+  );
+};
+
+export default Navigation;
