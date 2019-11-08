5a681a3bb195d8d5d59b0bdeeba6d48b595eddc8
kenetec <klhester3@gmail.com>
2019-11-07 22:46:00 -0600

    Add login detection funcationality

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,62 @@
-import React, { useState } from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 
+import PrivateRoute from './components/PrivateRoute';
 import AdminResourceManager from './components/AdminResourceManager';
 import Home from './components/Home';
 import Login from './components/Login';
+import Logout from './components/Logout';
 import Navigation from './components/Navigation';
 import NotFound from './components/NotFound';
 import Register from './components/Register';
 import ResourceDetail from './components/ResourceDetail';
 import Resources from './components/Resources';
 import ResourceUnknown from './components/ResourceUnknown';
+import { verify } from './utils/auth';
 
 const App = () => {
+  const [authed, setAuthed] = useState(false);
+
+  // componentDidMount
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setAuthed(true);
+    }
+
+    // validate token
+    verify(token, () => {});
+  }, []);
+
   return (
     <>
-      <Navigation />
+      <Navigation authed={authed} setAuthed={setAuthed} />
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/admin" exact component={AdminResourceManager} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/register" exact component={Register} />
+          <PrivateRoute
+            path="/admin"
+            component={AdminResourceManager}
+            authed={authed}
+            setAuthed={setAuthed}
+          />
+          <Route
+            path="/login"
+            render={() => <Login authed={authed} setAuthed={setAuthed} />}
+          />
+          <Route
+            path="/logout"
+            render={() => <Logout authed={authed} setAuthed={setAuthed} />}
+          />
+          <Route
+            path="/register"
+            render={() => <Register authed={authed} setAuthed={setAuthed} />}
+          />
           <Route path="/resources" exact component={Resources} />
           <Route path="/resources/unknown" component={ResourceUnknown} />
           <Route path="/resources/:id" component={ResourceDetail} />
