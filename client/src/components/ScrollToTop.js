fe77b14ba6eca2668f4847a4b3bc96b87ec4bfb2
Jacqueline Osborn <jackieo5023@gmail.com>
2019-11-13 16:05:05 -0600

    Add scrolltotop

---
+++ client/src/components/ScrollToTop.js
@@ -0,0 +1,13 @@
+// https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/guides/scroll-restoration.md
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
