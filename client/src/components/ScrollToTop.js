3bd0483c89bf324e851caceb28ec35c8ff5a22fc
Jacqueline Osborn <jackieo5023@gmail.com>
2019-11-13 16:11:28 -0600

    Fix lint

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -1,6 +1,10 @@
-// https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/guides/scroll-restoration.md
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+/**
+ * https://github.com/ReactTraining/react-router/blob/master/packages
+ * /react-router-dom/docs/guides/scroll-restoration.md
+ * */
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function ScrollToTop() {
   const { pathname } = useLocation();
