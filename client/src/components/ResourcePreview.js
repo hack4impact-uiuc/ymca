af847fbe2d8c811c20ff5c693078c6180c1d61f8
Jacqueline <jackieo5023@gmail.com>
2019-11-15 20:42:20 -0600

    Fix litn

--- client/src/components/ResourcePreview.js
+++ client/src/components/ResourcePreview.js
@@ -3,68 +3,78 @@ import PropTypes from 'prop-types';
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 
-import '../css/ResourcePreview.css'
+import '../css/ResourcePreview.css';
 
 const { Meta } = Card;
 
 function ResourcePreview(props) {
-  const { availableLanguages, category, cost, id, city, name, subcategory } = props;
-  const [src, setSrc] = useState("");
+  const {
+    availableLanguages,
+    category,
+    cost,
+    id,
+    city,
+    name,
+    subcategory,
+  } = props;
+  const [src, setSrc] = useState('');
 
   useEffect(() => {
     switch (subcategory) {
-      case "Abuse/Neglect":
-        setSrc("/asset/subcategories/abuse.jpg")
+      case 'Abuse/Neglect':
+        setSrc('/asset/subcategories/abuse.jpg');
         break;
-      case "Adult Education":
-        setSrc("/asset/subcategories/adultEd.jpg")
+      case 'Adult Education':
+        setSrc('/asset/subcategories/adultEd.jpg');
         break;
-      case "Drugs/Alcohol":
-        setSrc("/asset/subcategories/alcohol.jpg");
+      case 'Drugs/Alcohol':
+        setSrc('/asset/subcategories/alcohol.jpg');
         break;
-      case "Animal Care":
-        setSrc("/asset/subcategories/animalhealth.jpg");
+      case 'Animal Care':
+        setSrc('/asset/subcategories/animalhealth.jpg');
         break;
-      case "Children Education":
-        setSrc("/asset/subcategories/childEd.jpg");
+      case 'Children Education':
+        setSrc('/asset/subcategories/childEd.jpg');
         break;
-      case "Children":
-        setSrc("/asset/subcategories/childhealth.jpg")
+      case 'Children':
+        setSrc('/asset/subcategories/childhealth.jpg');
         break;
-      case "Clothing Assistance":
-        setSrc("/asset/subcategories/clothing.jpg")
+      case 'Clothing Assistance':
+        setSrc('/asset/subcategories/clothing.jpg');
         break;
-      case "Community Information":
-        setSrc("/asset/subcategories/community.jpg")
+      case 'Community Information':
+        setSrc('/asset/subcategories/community.jpg');
         break;
-      case "Dental":
-        setSrc("/asset/subcategories/dentalhealth.jpg")
+      case 'Dental':
+        setSrc('/asset/subcategories/dentalhealth.jpg');
         break;
-      case "Employment":
-        setSrc("/asset/subcategories/employment.jpg")
+      case 'Employment':
+        setSrc('/asset/subcategories/employment.jpg');
         break;
-      case "Vision":
-        setSrc("/asset/subcategories/eyehealth.jpg")
+      case 'Vision':
+        setSrc('/asset/subcategories/eyehealth.jpg');
         break;
-      case "Finance/Tax Assistance":
-        setSrc("/asset/subcategories/financialtax.jpg")
+      case 'Finance/Tax Assistance':
+        setSrc('/asset/subcategories/financialtax.jpg');
         break;
-      case "Medical":
-        setSrc("/asset/subcategories/medical.jpg")
+      case 'Medical':
+        setSrc('/asset/subcategories/medical.jpg');
         break;
-      case "Mental Health & Counseling":
-        setSrc("/asset/subcategories/mentalhealth.jpg")
+      case 'Mental Health & Counseling':
+        setSrc('/asset/subcategories/mentalhealth.jpg');
         break;
-      case "Students":
-        setSrc("/asset/subcategories/studentfinance.jpg")
+      case 'Students':
+        setSrc('/asset/subcategories/studentfinance.jpg');
         break;
       default:
-        setSrc(category === "Citizenship" ? "/asset/subcategories/citizenship.jpg" : "https://uiuc.hack4impact.org/static/images/team-cheer.jpg")
+        setSrc(
+          category === 'Citizenship'
+            ? '/asset/subcategories/citizenship.jpg'
+            : 'https://uiuc.hack4impact.org/static/images/team-cheer.jpg',
+        );
         break;
     }
-    return;
-
-  }, [category, setSrc, subcategory])
+  }, [category, setSrc, subcategory]);
 
   const description = [];
   let languages = '';
@@ -81,10 +91,15 @@ function ResourcePreview(props) {
   return (
     <Link to={`resources/${id}`}>
       <Card
-        cover={<div className="resource-preview-cover"><img className="resource-preview-cover-img"
-          alt={subcategory}
-          src={src}
-        /></div>}
+        cover={
+          <div className="resource-preview-cover">
+            <img
+              className="resource-preview-cover-img"
+              alt={subcategory}
+              src={src}
+            />
+          </div>
+        }
       >
         <Meta title={name} description={description} />
       </Card>
@@ -94,10 +109,12 @@ function ResourcePreview(props) {
 
 ResourcePreview.propTypes = {
   availableLanguages: PropTypes.arrayOf(PropTypes.string).isRequired,
+  category: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
   cost: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  subcategory: PropTypes.string.isRequired,
 };
 
 export default ResourcePreview;
