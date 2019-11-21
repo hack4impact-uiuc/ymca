a89a734f8df422b21ce144a6b65721c84877d39b
Jacqueline <jackieo5023@gmail.com>
2019-11-21 02:12:49 -0600

    Fix stock photos

--- client/src/components/ResourcePreview.js
+++ client/src/components/ResourcePreview.js
@@ -20,59 +20,42 @@ function ResourcePreview(props) {
   const [src, setSrc] = useState('');
 
   useEffect(() => {
-    switch (subcategory) {
-      case 'Abuse/Neglect':
-        setSrc('/asset/subcategories/abuse.jpg');
-        break;
-      case 'Adult Education':
-        setSrc('/asset/subcategories/adultEd.jpg');
-        break;
-      case 'Drugs/Alcohol':
-        setSrc('/asset/subcategories/alcohol.jpg');
-        break;
-      case 'Animal Care':
-        setSrc('/asset/subcategories/animalhealth.jpg');
-        break;
-      case 'Children Education':
-        setSrc('/asset/subcategories/childEd.jpg');
-        break;
-      case 'Children':
-        setSrc('/asset/subcategories/childhealth.jpg');
-        break;
-      case 'Clothing Assistance':
-        setSrc('/asset/subcategories/clothing.jpg');
-        break;
-      case 'Community Information':
-        setSrc('/asset/subcategories/community.jpg');
-        break;
-      case 'Dental':
-        setSrc('/asset/subcategories/dentalhealth.jpg');
-        break;
-      case 'Employment':
-        setSrc('/asset/subcategories/employment.jpg');
-        break;
-      case 'Vision':
-        setSrc('/asset/subcategories/eyehealth.jpg');
-        break;
-      case 'Finance/Tax Assistance':
-        setSrc('/asset/subcategories/financialtax.jpg');
-        break;
-      case 'Medical':
-        setSrc('/asset/subcategories/medical.jpg');
-        break;
-      case 'Mental Health & Counseling':
-        setSrc('/asset/subcategories/mentalhealth.jpg');
-        break;
-      case 'Students':
-        setSrc('/asset/subcategories/studentfinance.jpg');
-        break;
-      default:
-        setSrc(
-          category === 'Citizenship'
-            ? '/asset/subcategories/citizenship.jpg'
-            : 'https://uiuc.hack4impact.org/static/images/team-cheer.jpg',
-        );
-        break;
+    if (subcategory.includes('Abuse/Neglect')) {
+      setSrc('/asset/subcategories/abuse.jpg');
+    } else if (subcategory.includes('Adult Education')) {
+      setSrc('/asset/subcategories/adultEd.jpg');
+    } else if (subcategory.includes('Drugs/Alcohol')) {
+      setSrc('/asset/subcategories/alcohol.jpg');
+    } else if (subcategory.includes('Animal Care')) {
+      setSrc('/asset/subcategories/animalhealth.jpg');
+    } else if (subcategory.includes('Children Education')) {
+      setSrc('/asset/subcategories/childEd.jpg');
+    } else if (subcategory.includes('Children')) {
+      setSrc('/asset/subcategories/childhealth.jpg');
+    } else if (subcategory.includes('Clothing Assistance')) {
+      setSrc('/asset/subcategories/clothing.jpg');
+    } else if (subcategory.includes('Community Information')) {
+      setSrc('/asset/subcategories/community.jpg');
+    } else if (subcategory.includes('Dental')) {
+      setSrc('/asset/subcategories/dentalhealth.jpg');
+    } else if (subcategory.includes('Employment')) {
+      setSrc('/asset/subcategories/employment.jpg');
+    } else if (subcategory.includes('Vision')) {
+      setSrc('/asset/subcategories/eyehealth.jpg');
+    } else if (subcategory.includes('Finance/Tax Assistance')) {
+      setSrc('/asset/subcategories/financialtax.jpg');
+    } else if (subcategory.includes('Medical')) {
+      setSrc('/asset/subcategories/medical.jpg');
+    } else if (subcategory.includes('Mental Health & Counseling')) {
+      setSrc('/asset/subcategories/mentalhealth.jpg');
+    } else if (subcategory.includes('Students')) {
+      setSrc('/asset/subcategories/studentfinance.jpg');
+    } else {
+      setSrc(
+        category.includes('Citizenship')
+          ? '/asset/subcategories/citizenship.jpg'
+          : 'https://uiuc.hack4impact.org/static/images/team-cheer.jpg',
+      );
     }
   }, [category, setSrc, subcategory]);
 
@@ -109,12 +92,12 @@ function ResourcePreview(props) {
 
 ResourcePreview.propTypes = {
   availableLanguages: PropTypes.arrayOf(PropTypes.string).isRequired,
-  category: PropTypes.string.isRequired,
+  category: PropTypes.arrayOf(PropTypes.string).isRequired,
   city: PropTypes.string.isRequired,
   cost: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  subcategory: PropTypes.string.isRequired,
+  subcategory: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default ResourcePreview;
