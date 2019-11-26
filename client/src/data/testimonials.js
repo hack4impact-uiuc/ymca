2fd41e1c72318f56852feaaf920b33b8b90099cd
anniegw2 <annie.wu701@gmail.com>
2019-11-26 16:34:24 -0600

    Add three ymca testimonials

--- client/src/data/testimonials.js
+++ client/src/data/testimonials.js
@@ -1,44 +1,41 @@
 export default [
   {
-    person: 'Kendalyn Hesterson',
-    picture:
-      'https://media.licdn.com/dms/image/C5603AQE4fiq2wYLN4w/' +
-      'profile-displayphoto-shrink_200_200/0?e=1575504000&v=beta&' +
-      't=ENWJca_rj3uyCKSjckj0IkVQj-Z__6hCr9-rSIDvQOc',
-    country: 'Switzerland',
-    testimonial: `Lorem ipsum dolor amet mustache knausgaard +1, 
-            blue bottle waistcoat tbh semiotics artisan synth stumptown 
-            gastropub cornhole celiac swag. Brunch raclette vexillologist 
-            post-ironic glossier ennui XOXO mlkshk godard pour-over blog 
-            tumblr humblebrag. Blue bottle put a bird on it twee prism 
-            biodiesel brooklyn. Blue bottle ennui tbh succulents.`,
+    person: 'Daniella Di Stefano',
+    picture: '/asset/images/daniella_stefano.JPG',
+    // 'profile-displayphoto-shrink_200_200/0?e=1575504000&v=beta&' +
+    // 't=ENWJca_rj3uyCKSjckj0IkVQj-Z__6hCr9-rSIDvQOc',
+    country: 'Legal Services Intern',
+    testimonial: `"The Welcome Center has become a home
+                  to me. I understood I was an ally of the
+                  immigrant population, but I was sedentary in
+                  my actions which made me part of the problem.
+                  I saw this opportunity to become active in
+                  how I felt, not just think about it, and
+                  do something about it. Now I'm here."`,
   },
   {
-    person: 'Neeraj Aggarwal',
-    picture:
-      'https://media.licdn.com/dms/image/C5603AQEvEZTMzhTq4w/' +
-      'profile-displayphoto-shrink_800_800/0?e=1577923200&v=beta&' +
-      't=kkhlkYaFc_v6QEnSaWYfPFXOGHVU8qARMo60j6lXMB8',
-    country: 'France',
-    testimonial: `Lorem ipsum dolor amet mustache knausgaard +1, 
-            blue bottle waistcoat tbh semiotics artisan synth stumptown 
-            gastropub cornhole celiac swag. Brunch raclette vexillologist 
-            post-ironic glossier ennui XOXO mlkshk godard pour-over blog 
-            tumblr humblebrag. Blue bottle put a bird on it twee prism 
-            biodiesel brooklyn. Blue bottle ennui tbh succulents.`,
+    person: 'Michele Biscene',
+    picture: '/asset/images/michele_biscene.JPG',
+    // 'profile-displayphoto-shrink_800_800/0?e=1577923200&v=beta&' +
+    // 't=kkhlkYaFc_v6QEnSaWYfPFXOGHVU8qARMo60j6lXMB8',
+    country: 'NAWC Program Participant',
+    testimonial: `"I wanted to apply for citizenship. I didn't
+                    know what to do. I didn't have any idea. YMCA
+                    helped me fill those papers. It's taken away all
+                    the stress I had before, all the insecurities I had.
+                    I know the door I can knock to for advice and ideas
+                    on what I can do. "`,
   },
   {
-    person: 'Annie Gu',
-    picture:
-      'https://media.licdn.com/dms/image/C5603AQHtN2qYAwV4pw/' +
-      'profile-displayphoto-shrink_800_800/0?e=1577923200&v=beta&' +
-      't=tGzGaXGBwFs5BHiOg11JkTCpMl5lgzmYiIGwApypizM',
-    country: 'Turkey',
-    testimonial: `Lorem ipsum dolor amet mustache knausgaard +1, 
-            blue bottle waistcoat tbh semiotics artisan synth stumptown 
-            gastropub cornhole celiac swag. Brunch raclette vexillologist 
-            post-ironic glossier ennui XOXO mlkshk godard pour-over blog 
-            tumblr humblebrag. Blue bottle put a bird on it twee prism 
-            biodiesel brooklyn. Blue bottle ennui tbh succulents.`,
+    person: 'Gloria Yen',
+    picture: '/asset/images/gloria_yen.JPG',
+    // 'profile-displayphoto-shrink_800_800/0?e=1577923200&v=beta&' +
+    // 't=tGzGaXGBwFs5BHiOg11JkTCpMl5lgzmYiIGwApypizM',
+    country: 'NAWC Director',
+    testimonial: `"By engaging different elected officials, public sector
+            institutions, social service providers, faith communities,
+            business community, and also immigrant leaders, we're able to
+            create multi-sector collaborations that foster welcoming inclusion
+            and access to meaningful communities."`,
   },
 ];
