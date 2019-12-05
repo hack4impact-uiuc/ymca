a6ff5554c26db6faec74b5ffd09dc4243031407c
kenetec <klhester3@gmail.com>
2019-12-04 21:42:14 -0600

    Add InternalNote schema and edit FinancialAid schema

--- api/src/models/resource.js
+++ api/src/models/resource.js
@@ -17,10 +17,12 @@ const FinancialAid = new mongoose.Schema({
   education: { type: String, required: false },
   immigrationStatus: { type: String, required: false },
   deadline: { type: String, required: false },
+  amount: {type: String, required: false},
 });
 
 const InternalNote = new mongoose.Schema({
-  
+  subject: {type: String, required: true},
+  body: {type: String, required: true},
 });
 
 const Resource = new mongoose.Schema({
@@ -46,7 +48,7 @@ const Resource = new mongoose.Schema({
     required: false,
   },
   comments: { type: [String], required: false },
-  internalNotes: { type: [String], required: false },
+  internalNotes: { type: [InternalNote], required: false },
 });
 
 module.exports = mongoose.model('Resource', Resource);
