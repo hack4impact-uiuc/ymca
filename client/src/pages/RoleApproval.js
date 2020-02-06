47234f3d4e3b08073e9aaf12e384026e9eaf4865
Jacqueline Osborn <jackieo5023@gmail.com>
2020-02-05 18:13:49 -0600

    Conversion

--- client/src/pages/RoleApproval.js
+++ client/src/pages/RoleApproval.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {
   Table,
   Card,
@@ -15,132 +15,129 @@ import {
 
 import { getUsersForRolesPage, changeRole } from '../utils/auth';
 
-class RoleApproval extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-      newRole: '',
-      userWithNewRole: -1,
-      password: '',
-    };
-  }
+const RoleApproval = () => {
+  const [newRole, setNewRole] = useState('');
+  const [response, setResponse] = useState('');
+  const [password, setPassword] = useState('');
+  const [users, setUsers] = useState([]);
+  const [userWithNewRole, setUserWithNewRole] = useState(-1);
 
-  async componentDidMount() {
-    const userData = await getUsersForRolesPage();
-    const userDataParsed = await userData.json();
-    if (userDataParsed.user_emails) {
-      this.setState({ users: userDataParsed.user_emails });
+  useEffect(() => {
+    async function fetchData() {
+      const userData = await getUsersForRolesPage();
+      const userDataParsed = await userData.json();
+
+      if (userDataParsed.user_emails) {
+        setUsers(userDataParsed.user_emails);
+      }
     }
-  }
+    fetchData();
+  }, [setUsers]);
 
-  setNewRole = (newRole, userWithNewRole) => () => {
-    this.setState({ newRole, userWithNewRole });
-  };
+  const setNewRoleAndUser = useCallback(
+    (newRoleToSet, userWithNewRoleToSet) => {
+      setNewRole(newRoleToSet);
+      setUserWithNewRole(userWithNewRoleToSet);
+    },
+    [setNewRole, setUserWithNewRole],
+  );
 
-  submitNewRole = async event => {
+  const submitNewRole = async event => {
     event.preventDefault();
     const changeRoleData = await changeRole(
-      this.state.users[this.state.userWithNewRole].email,
-      this.state.newRole,
-      this.state.password,
+      users[userWithNewRole].email,
+      newRole,
+      password,
     );
-    const changeRoleDataParsed = await changeRoleData.json();
-    const userData = await getUsersForRolesPage();
+
+    const [changeRoleDataParsed, userData] = await Promise.all([
+      changeRoleData.json(),
+      getUsersForRolesPage(),
+    ]);
     const userDataParsed = await userData.json();
-    this.setState({
-      users: userDataParsed.user_emails,
-      userWithNewRole: -1,
-      newRole: '',
-      response: changeRoleDataParsed.message,
-    });
+
+    setNewRole('');
+    setResponse(changeRoleDataParsed.message);
+    setUsers(userDataParsed.user_emails);
+    setUserWithNewRole(-1);
   };
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-
-  render() {
-    return (
-      <div align="center">
-        <Card
-          className="interview-card"
-          style={{ height: '60%', margin: '2%' }}
-        >
-          <CardBody>
-            <Table hover>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Email</th>
-                  <th>Role</th>
-                  <th>Change Role</th>
-                  <th> </th>
+  return (
+    <div align="center">
+      <Card className="interview-card" style={{ height: '60%', margin: '2%' }}>
+        <CardBody>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Change Role</th>
+                <th> </th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, idx) => (
+                <tr key={user.email}>
+                  <th scope="row">{idx + 1}</th>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <UncontrolledDropdown>
+                      <DropdownToggle caret>
+                        {idx === userWithNewRole ? newRole : 'New Role'}
+                      </DropdownToggle>
+                      <DropdownMenu color="info">
+                        <DropdownItem
+                          onClick={() => setNewRoleAndUser('public', idx)}
+                        >
+                          Public
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => setNewRoleAndUser('intern', idx)}
+                        >
+                          Intern
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => setNewRoleAndUser('admin', idx)}
+                        >
+                          Admin
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </td>
+                  <td>
+                    <Button
+                      color="info"
+                      size="sm"
+                      onClick={event => submitNewRole(event)}
+                    >
+                      Submit
+                    </Button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {this.state.users.map((user, idx) => (
-                  <tr key={user.email}>
-                    <th scope="row">{idx + 1}</th>
-                    <td>{user.email}</td>
-                    <td>{user.role}</td>
-                    <td>
-                      <UncontrolledDropdown>
-                        <DropdownToggle caret>
-                          {idx === this.state.userWithNewRole
-                            ? this.state.newRole
-                            : 'New Role'}
-                        </DropdownToggle>
-                        <DropdownMenu color="info">
-                          <DropdownItem
-                            onClick={this.setNewRole('public', idx)}
-                          >
-                            Public
-                          </DropdownItem>
-                          <DropdownItem
-                            onClick={this.setNewRole('intern', idx)}
-                          >
-                            Intern
-                          </DropdownItem>
-                          <DropdownItem onClick={this.setNewRole('admin', idx)}>
-                            Admin
-                          </DropdownItem>
-                        </DropdownMenu>
-                      </UncontrolledDropdown>
-                    </td>
-                    <td>
-                      <Button
-                        color="info"
-                        size="sm"
-                        onClick={this.submitNewRole}
-                      >
-                        Submit
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-            {!localStorage.getItem('google') ? (
-              <div align="left" style={{ display: 'inline', width: '300px' }}>
-                <FormGroup size="sm">
-                  <Label for="examplePassword">Confirm Password</Label>
-                  <Input
-                    type="password"
-                    name="password"
-                    maxLength="128"
-                    value={this.state.password}
-                    onChange={this.handleChange}
-                  />
-                </FormGroup>
-              </div>
-            ) : null}
-          </CardBody>
-        </Card>
-        {this.state.response}
-      </div>
-    );
-  }
-}
+              ))}
+            </tbody>
+          </Table>
+          {!localStorage.getItem('google') ? (
+            <div align="left" style={{ display: 'inline', width: '300px' }}>
+              <FormGroup size="sm">
+                <Label for="examplePassword">Confirm Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  maxLength="128"
+                  value={password}
+                  onChange={event => setPassword(event.target.value)}
+                />
+              </FormGroup>
+            </div>
+          ) : null}
+        </CardBody>
+      </Card>
+      {response}
+    </div>
+  );
+};
 
 export default RoleApproval;
