309206bf5b02530475a9bb3e1b7d3a06f1daad82
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-02-25 18:51:41 -0600

    Add test

--- api/test/resource.test.js
+++ api/test/resource.test.js
@@ -2,6 +2,10 @@ const app = require('../src/app');
 const mongoose = require('mongoose');
 const request = require('supertest');
 
+beforeEach(() => {
+  // TODO: drop database
+});
+
 describe('GET / ', () => {
   test('API should return working message', async () => {
     const response = await request(app).get('/');
@@ -10,7 +14,17 @@ describe('GET / ', () => {
   });
 });
 
+describe('GET /api/resources/ ', () => {
+  test('API should return working message', async () => {
+    const response = await request(app).get('/api/resources/');
+    expect(response.body.message).toEqual('');
+    expect(response.body.result).toEqual([]);
+    expect(response.statusCode).toBe(200);
+  });
+});
+
 afterAll(async done => {
+  // TODO: drop database
   await mongoose.connection.close();
   done();
 });
