988ab995c6ecb4a0a8352278ced816d6fbee5dff
Albert Cao <albertc4@illinois.edu>
2020-02-25 23:35:28 -0600

    working on adding resource to user endpoint

--- api/src/api/user.js
+++ api/src/api/user.js
@@ -1,33 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const { errorWrap } = require('../middleware');
-const User = require('../../../auth/src/models/User')
-const Resource = require('../models/resource');
-const { verifyUser } = require("../../../utils/userVerification");
-const { sendResponse } = require("../../../utils/sendResponse");
+const User = require('../../../auth/src/models/User');
+const { jwt_decode } = require('jwt-decode');
 
 // Add resource to saved resources of user
 router.get(
     '/',
     errorWrap(async (req, res) => {
-        return sendResponse(res, 200, "HELLLOOEFJIWEOFJ:OFWFOWJFI:OWJFE:IOWFEIJO");
+        res.status(200).json({
+            code: 200,
+            message: `Successfully called get lol`,
+            success: true,
+        })
     }),
 );
 
 router.put(
     '/',
     errorWrap(async (req, res) => {
-        const { resourceId } = req.params;
-        if (!req.headers.token) {
-            return sendResponse(res, 400, "Invalid Token");
-        }
         // Verifies the token and checks that the user is registered
-        const user = await verifyUser(req.headers.token);
-        if (user.errorMessage != null) {
-            return sendResponse(res, 400, user.errorMessage);
-        }
+        const { resourceId } = req.params;
+        const userId = jwt_decode(req.headers.token);
+        const user = await User.findById(userId);
         user.savedResources.push(resourceId);
-        sendResponse(res, 200, "Successful Addition of Resource!");
+        user.save()
+        res.status(200).json({
+            code: 200,
+            message: `Successfully added saved resource`,
+            success: true,
+            result: resourceId,
+        });
     }),
 );
   
