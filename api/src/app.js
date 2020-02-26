5b3314006ac4754dfd061020e2c22e2f8cc2458c
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-02-25 18:04:23 -0600

    Reorganize app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,7 @@
+const app = express();
 const createError = require('http-errors');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const express = require('express');
 const fs = require('fs');
 const helmet = require('helmet');
@@ -7,15 +9,23 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes');
-const dotenv = require('dotenv');
+
+const { errorHandler } = require('./middleware');
 
 dotenv.config({
   path: path.resolve(__dirname, `../config/${process.env.NODE_ENV}.env`),
 });
 
-const { errorHandler } = require('./middleware');
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const app = express();
+mongoose.Promise = global.Promise;
+
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(helmet());
 app.use(cors());
@@ -29,20 +39,8 @@ app.use('/', routes);
 
 app.get('/', (req, res) => res.json('API working!'));
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.Promise = global.Promise;
-
-if (process.env.NODE_ENV !== 'test') {
-  mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error));
-}
-
 app.get('/favicon.ico', (req, res) => res.status(204));
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
--- api/test/resource.test.js
+++ api/test/resource.test.js
@@ -1,11 +1,9 @@
-process.env.NODE_ENV = 'test';
-
-const request = require('supertest');
 const app = require('../src/app');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const request = require('supertest');
 
 describe('GET / ', () => {
-  test('It should respond with an array of students', async () => {
+  test('API should return working message', async () => {
     const response = await request(app).get('/');
     expect(response.body).toEqual('API working!');
     expect(response.statusCode).toBe(200);
@@ -13,7 +11,6 @@ describe('GET / ', () => {
 });
 
 afterAll(async done => {
-  // dbConnection.close();
-  await mongoose.connection.close()
+  await mongoose.connection.close();
   done();
 });
