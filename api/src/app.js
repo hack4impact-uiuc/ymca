b94fc842602b65764a70d42b32b1f86d7a5978a6
Neeraj Aggarwal <hello@neerajaggarwal.com>
2020-02-25 21:13:41 -0600

    🔒 Backend test framework setup (#139)

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const express = require('express');
 const fs = require('fs');
 const helmet = require('helmet');
@@ -8,9 +9,25 @@ const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes');
 
+const app = express();
 const { errorHandler } = require('./middleware');
 
-const app = express();
+if (process.env.NODE_ENV != 'production') {
+  dotenv.config({
+    path: path.resolve(__dirname, `../config/${process.env.NODE_ENV}.env`),
+  });
+}
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.Promise = global.Promise;
+
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(helmet());
 app.use(cors());
@@ -22,16 +39,10 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/', routes);
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-mongoose.Promise = global.Promise;
-mongoose.connection
-  .once('open', () => console.log('Connected to MongoLab instance.'))
-  .on('error', error => console.log('Error connecting to MongoLab:', error));
+app.get('/', (req, res) => res.json('API working!'));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
