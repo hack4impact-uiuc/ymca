c7a508aed823f7db92f8109fcb424ac49fb9dd46
kenetec <klhester3@gmail.com>
2020-03-31 02:55:04 -0500

    Fix invisible admin form bug

--- client/src/components/ResourceForm.js
+++ client/src/components/ResourceForm.js
@@ -164,7 +164,7 @@ const ResourceForm = (props: FormProps) => {
   );
 
   return (
-    <Layout className="resource-form">
+    <Layout className="resource-form-layout">
       <Header className="header">
         <Row justify="center" type="flex">
           <h2>Add a Resource</h2>
@@ -175,7 +175,7 @@ const ResourceForm = (props: FormProps) => {
           {createCarouselCategories()}
         </Carousel>
         <Form
-          className="form"
+          className="resource-form"
           onSubmit={e => {
             onSubmitNewResourceForm(e, totalSubmitEnabled, id, {
               category: categories,
