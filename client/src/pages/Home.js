b527f19660dcfcb385836aee5f123c884be613d7
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2020-04-09 21:52:13 -0500

    now hello?

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -45,8 +45,8 @@ const Home = () => {
                   className="home-block-4-title"
                   style={{
                     paddingTop: `${isMobile ? '2em' : '.5em'}`,
-                    fontSize: `${isMobile ? '0.32em' : '0.45em'}`,
                     paddingBottom: '.5em',
+                    fontSize: `${isMobile ? '0.32em' : '0.45em'}`,
                   }}
                 >
                   Partners in the Community
