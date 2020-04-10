d49f82c5326d2685dd21da3b76037464829c02dc
Anooj Lal <anoojl2@illinois.edu>
2020-04-09 22:10:28 -0400

    🌟 Resource rating indicated by stars (#234)

--- client/src/pages/ResourceDetail.js
+++ client/src/pages/ResourceDetail.js
@@ -40,6 +40,7 @@ export default class ResourceDetail extends Component {
       internalNotes: [],
       hours: [],
       isSaved: false,
+      recommendation: 0,
     };
   }
 
@@ -72,6 +73,7 @@ export default class ResourceDetail extends Component {
         hours: result.hoursOfOperation
           ? result.hoursOfOperation.hoursOfOperation
           : [],
+        recommendation: result.recommendation,
       });
 
       if (this.props.authed) {
@@ -157,7 +159,6 @@ export default class ResourceDetail extends Component {
       city,
       state,
       zip,
-      email,
       website,
       description,
       languages,
@@ -172,6 +173,7 @@ export default class ResourceDetail extends Component {
       internalNotes,
       hours,
       isSaved,
+      recommendation,
     } = this.state;
 
     const Map = ReactMapboxGl({
@@ -207,6 +209,20 @@ export default class ResourceDetail extends Component {
       }
     }
 
+    const stars = [];
+
+    if (recommendation != null) {
+      for (let i = 0; i < recommendation; i += 1) {
+        stars.push(<Icon type="star" theme="filled" className="filled-star" />);
+      }
+
+      for (let i = 0; i < 5 - recommendation; i += 1) {
+        stars.push(
+          <Icon type="star" theme="filled" className="unfilled-star" />,
+        );
+      }
+    }
+
     return (
       <div className="resource-detail">
         <Modal
@@ -229,7 +245,8 @@ export default class ResourceDetail extends Component {
         </Header>
         <Row className="section">
           <Col span={15}>
-            <span className="resource-name">{name}</span>
+            <span className="resource-name">{`${name}\n`}</span>
+            {recommendation != null && stars}
             <SaveButton
               authed={authed}
               isSaved={isSaved}
