499e74624f2aa3f73794713f60ce8c374691655f
Albert Cao <albertc4@illinois.edu>
2020-04-14 22:37:51 -0500

    styling changes

--- client/src/components/SaveButton.js
+++ client/src/components/SaveButton.js
@@ -1,53 +1,59 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom/';
 import PropTypes from 'prop-types';
-import { Button, Icon, Modal } from 'antd';
+import { Button, Icon, Popover } from 'antd';
 
 function SaveButton(props) {
   const { authed, isSaved, deleteResourceHandler, saveResourceHandler } = props;
 
   const [saveError, setSaveError] = useState(false);
 
+  const errorContent = (
+    <div>
+      You must be logged in to use this feature! Login{' '}
+      <Link to="/login">here</Link>.
+    </div>
+  );
+
   return (
-    <a onClick={e => e.preventDefault()}>
-      <Modal
-        title="Error"
-        closable={false}
-        cancelButtonProps={{ style: { display: 'none' } }}
-        visible={saveError}
-        onOk={() => setSaveError(false)}
-      >
-        You must be logged in to use this feature! Login{' '}
-        <Link to="/login">here</Link>.
-      </Modal>
-      {isSaved ? (
-        <Button
-          onClick={async () => {
-            await deleteResourceHandler();
-          }}
-          type="link"
-        >
-          <Icon
-            type="star"
-            theme="filled"
-            style={{ fontSize: '16px', color: '#562996' }}
-          />
-        </Button>
+    <>
+      {!authed ? (
+        <Popover content={errorContent} Title="Error" trigger="click">
+          <Button type="link">
+            <Icon type="star" style={{ fontSize: '16px', color: '#562996' }} />
+          </Button>
+        </Popover>
       ) : (
-        <Button
-          onClick={async () => {
-            if (authed) {
-              await saveResourceHandler();
-            } else {
-              setSaveError(true);
-            }
-          }}
-          type="link"
-        >
-          <Icon type="star" style={{ fontSize: '16px', color: '#562996' }} />
-        </Button>
+        <a onClick={e => e.preventDefault()}>
+          {isSaved ? (
+            <Button
+              onClick={async () => {
+                await deleteResourceHandler();
+              }}
+              type="link"
+            >
+              <Icon
+                type="star"
+                theme="filled"
+                style={{ fontSize: '16px', color: '#562996' }}
+              />
+            </Button>
+          ) : (
+            <Button
+              onClick={async () => {
+                await saveResourceHandler();
+              }}
+              type="link"
+            >
+              <Icon
+                type="star"
+                style={{ fontSize: '16px', color: '#562996' }}
+              />
+            </Button>
+          )}
+        </a>
       )}
-    </a>
+    </>
   );
 }
 
