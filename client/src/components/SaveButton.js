45c901b9136f5f0fddf7aab548d33451a4a4eec8
Albert Cao <albertc4@illinois.edu>
2020-04-16 20:07:15 -0500

    removed unnecessary import

--- client/src/components/SaveButton.js
+++ client/src/components/SaveButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom/';
 import PropTypes from 'prop-types';
 import { Button, Icon, Popover } from 'antd';
