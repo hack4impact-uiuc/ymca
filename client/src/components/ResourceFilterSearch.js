b07fad0bbfc05e38d0bab7748f3d6b3eea2f1c45
kenetec <khester4@illinois.edu>
2020-04-23 14:40:55 -0500

    Save

--- client/src/components/ResourceFilterSearch.js
+++ client/src/components/ResourceFilterSearch.js
@@ -1,13 +1,13 @@
 // @flow
 
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useReducer } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AutoComplete, Icon, Input } from 'antd';
 
 import { getResources } from '../utils/api';
 import '../css/ResourcesFilter.css';
 
-const { Option } = AutoComplete;
+const { Option, OptGroup } = AutoComplete;
 
 /*
 on search have the resource grid be populated with the filtered results here
@@ -15,17 +15,72 @@ on search have the resource grid be populated with the filtered results here
 const ResourceFilterSearch = () => {
   const history = useHistory();
 
-  const [allResourceOptions, setAllResourceOptions] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [resourceToCategories, setResourceToCategories] = useState({});
+  const [keysToShow, setKeysToShow] = useState([]);
 
-  const getResourceNames = useCallback(() => {
+  const populateOptions = useCallback(() => {
     getResources().then(res => {
       if (res !== null) {
         if (res.code === 200) {
-          setAllResourceOptions(
-            Object.values(res.result).map(resource => (
-              <Option key={resource.name} value={resource._id}>
-                {resource.name !== null ? resource.name : '_'}
-              </Option>
+          const optionsBlueprint = [];
+
+          Object.values(res.result).forEach(resource => {
+            resourceToCategories[resource.name] = {
+              category: resource.category,
+              subcategory: resource.subcategory,
+            };
+
+            resource.category.forEach((category, index) => {
+              const subcategory = resource.subcategory[index];
+
+              const filteredCategories = optionsBlueprint.filter(
+                group => group.title === category,
+              );
+              const categoryGroupExists = filteredCategories.length > 0;
+              const categoryGroup = categoryGroupExists
+                ? filteredCategories[0]
+                : { title: category, children: [] };
+
+              const filteredSubcategories = categoryGroup.children.filter(
+                group => group.title === subcategory,
+              );
+              const subcategoryGroupExists = filteredSubcategories.length > 0;
+              const subcategoryGroup = subcategoryGroupExists
+                ? filteredSubcategories[0]
+                : { title: subcategory, children: [] };
+
+              subcategoryGroup.children.push(resource.name);
+
+              if (!subcategoryGroupExists) {
+                categoryGroup.children.push(subcategoryGroup);
+              }
+
+              if (!categoryGroupExists) {
+                optionsBlueprint.push(categoryGroup);
+              }
+            });
+          });
+
+          setOptions(
+            optionsBlueprint.map(categoryGroup => (
+              <OptGroup
+                key={categoryGroup.title}
+                label={<a>{categoryGroup.title}</a>}
+              >
+                {categoryGroup.children.map(subcategoryGroup => (
+                  <OptGroup
+                    key={subcategoryGroup.title}
+                    label={<a>{subcategoryGroup.title}</a>}
+                  >
+                    {subcategoryGroup.children.map(resourceName => (
+                      <Option key={resourceName} label={resourceName}>
+                        {resourceName}
+                      </Option>
+                    ))}
+                  </OptGroup>
+                ))}
+              </OptGroup>
             )),
           );
         } else {
@@ -34,17 +89,37 @@ const ResourceFilterSearch = () => {
       }
       // also show error
     });
-  }, [setAllResourceOptions]);
+  }, [setOptions]);
 
-  const filterSearchResults = useCallback(
-    (input, option) =>
-      option.props.children
+  const existsInString = useCallback(
+    (source, target) =>
+      source
         .toUpperCase()
-        .substring(0, input.length)
-        .indexOf(input.toUpperCase()) !== -1 ||
-      option.props.children.toUpperCase().indexOf(input.toUpperCase()) !== -1,
+        .substring(0, target.length)
+        .indexOf(target.toUpperCase) !== -1 ||
+      source.toUpperCase().indexOf(target.toUpperCase()) !== -1,
     [],
   );
+
+  const filterSearchResults = useCallback((input, option) => {
+    if (option.type === Option && existsInString(option.key, input)) {
+      setKeysToShow([
+        ...keysToShow,
+        option.key,
+        ...resourceToCategories[option.key].category,
+        ...resourceToCategories[option.key].subcategory,
+      ]);
+      return true;
+    }
+    if (
+      option.type === OptGroup &&
+      (existsInString(option.key, input) || keysToShow.includes(option.key))
+    ) {
+      return true;
+    }
+    console.log(keysToShow);
+    return false;
+  }, []);
 
   const onSearchSelect = useCallback(
     value => {
@@ -53,13 +128,13 @@ const ResourceFilterSearch = () => {
     [history],
   );
 
-  useEffect(getResourceNames, []);
+  useEffect(populateOptions, []);
 
   return (
     <AutoComplete
       className="searchbar-filter"
-      dataSource={allResourceOptions}
       placeholder="Search for a Resource"
+      dataSource={options}
       filterOption={filterSearchResults}
       onSelect={onSearchSelect}
     >
