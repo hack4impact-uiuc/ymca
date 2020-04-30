f0bfb443fc68a3887404b56fdd42be8c9006719a
Neeraj Aggarwal <hello@neerajaggarwal.com>
2020-04-30 14:02:05 -0700

    🗞️ Deploy to nawc (#259)
    * Update new url
    
    * Change titles

--- api/src/appListener.js
+++ api/src/appListener.js
@@ -2,7 +2,7 @@ const app = require('./app');
 
 const API_PORT = process.env.PORT ? process.env.PORT : 9000;
 app.listen(API_PORT, async () =>
-  console.log(`YMCA API server listening on port ${API_PORT}!`),
+  console.log(`API server listening on port ${API_PORT}!`),
 );
 
 process.on('unhandledRejection', error => {
