a13f88d280efce11bb2ec6557aa32d69b42a406c
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2020-04-30 15:47:11 -0700

    Finish share button

--- client/src/components/ShareButton.js
+++ client/src/components/ShareButton.js
@@ -1,21 +1,25 @@
 // @flow
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 
 function ShareButton(props) {
   const location = useLocation();
 
+  const [tipText, setTipText] = useState('Copy resource link!');
+
   const copyLink = () => {
     const link = `https://nawc.now.sh${location.pathname}`;
     navigator.clipboard.writeText(link);
-    console.log('Link copied!');
+    setTipText('Resource link copied!');
   };
 
   return (
     <>
-      <Button onClick={() => copyLink()}>Share</Button>
+      <Tooltip title={tipText}>
+        <Button onClick={() => copyLink()}>Share</Button>
+      </Tooltip>
     </>
   );
 }
