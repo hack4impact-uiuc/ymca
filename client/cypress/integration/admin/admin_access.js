72102d744770a9d4dbe58ae5323a24626f10fe09
noa <aggarwal.neeraj141@gmail.com>
2020-12-08 15:54:07 -0800

    Add some admin tests

--- client/cypress/integration/admin/admin_access.js
+++ client/cypress/integration/admin/admin_access.js
@@ -5,8 +5,18 @@ context('Admin Access', () => {
     cy.visit('/');
   });
 
-  it('unauthenticated admin access redirects to login', () => {
+  it('unauthenticated /admin access redirects to login', () => {
     cy.visit('/admin');
+    cy.url().should('match', /login/);
+  });
+
+  it('unauthenticated /edit-home access redirects to login', () => {
+    cy.visit('/edit-home');
+    cy.url().should('match', /login/);
+  });
+
+  it('unauthenticated /role-approval access redirects to login', () => {
+    cy.visit('/role-approval');
     cy.url().should('match', /login/);
   });
 });
