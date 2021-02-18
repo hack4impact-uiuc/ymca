5da5d0314cc462c13b2f7516cd95750777e4bd96
Jacqueline <jackieo5023@gmail.com>
2021-02-18 00:12:17 -0600

    Downgrad react-mapbox-gl and remove bootstrap

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/index.css';
 import { ProvideAuth } from './utils/use-auth';
 
