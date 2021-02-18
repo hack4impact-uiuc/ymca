543c24c1707e8baabbaf9bf56b7d6b1863532472
Jacqueline <jackieo5023@gmail.com>
2021-02-18 00:22:45 -0600

    Add bootstrap back in

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/index.css';
 import { ProvideAuth } from './utils/use-auth';
 
