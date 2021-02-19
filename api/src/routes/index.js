83f9620a58668391a98638021121e361c285baac
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2021-02-19 16:02:09 -0600

    index stuff

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
-const { admin, category, resource, homepage } = require('../api');
+const { admin, category, resource, homepage, translation } = require('../api');
 const { authAdmin, authGeneral } = require('../middleware/auth');
 
 router.use('/api/categories', category);
 router.use('/api/resources', resource);
 router.use('/api/admin', authAdmin, admin);
 router.use('/api/homepage', homepage);
+router.use('/api/translation', translation);
 
 module.exports = router;
