c719de6e753d82f7a2c6f244b723254b5809c182
Mallik <MALLIAX1@abbvienet.com>
2021-03-15 19:17:27 -0500

    Fix icon styling and implement arrow animation

--- client/src/components/ResourceManager.jsx
+++ client/src/components/ResourceManager.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { Menu } from 'antd';
 import {
   DownOutlined,
+  UpOutlined,
   EditOutlined,
   CloseOutlined,
   PlusOutlined,
@@ -13,8 +14,60 @@ import { getCategories } from '../utils/api';
 
 import '../css/ResourceManager.css';
 
-const ResourceManager = () => {
+type Props = {
+  categories: { [string]: Array<string> },
+  categoryName: String,
+};
+
+const SidebarCategory = (props: Props) => {
   const { SubMenu } = Menu;
+
+  const { categories, categoryName } = props;
+  const [opened, setOpened] = useState(false);
+
+  const handleChange = () => {
+    setOpened(!opened);
+  };
+
+  return (
+    <SubMenu
+      {...props}
+      title={
+        <span>
+          {categoryName}
+          <span style={{ position: 'absolute', right: 15 }}>
+            <EditOutlined />
+            <CloseOutlined style={{ color: '#FF0000' }} />
+          </span>
+        </span>
+      }
+      icon={
+        opened ? (
+          <UpOutlined style={{ position: 'relative', top: -2 }} />
+        ) : (
+          <DownOutlined style={{ position: 'relative', top: -2 }} />
+        )
+      }
+      onTitleClick={handleChange}
+    >
+      {categories[categoryName].map((subCategory) => (
+        <Menu.Item key={subCategory} className="subcategory">
+          {subCategory}
+          <div>
+            <EditOutlined />
+            <CloseOutlined style={{ color: '#FF0000' }} />
+          </div>
+        </Menu.Item>
+      ))}
+      <Menu.Item key="Add Subcategory" className="subcategory">
+        Add Subcategory
+        <PlusOutlined style={{ position: 'relative', top: 10 }} />
+      </Menu.Item>
+    </SubMenu>
+  );
+};
+
+const ResourceManager = () => {
   const [categories, setCategories] = useState<{ [string]: Array<string> }>({});
 
   useEffect(() => {
@@ -35,33 +88,11 @@ const ResourceManager = () => {
   return (
     <Menu style={{ width: 280 }} mode="inline" expandIcon={<div />}>
       {Object.keys(categories).map((categoryName) => (
-        <SubMenu
+        <SidebarCategory
+          categories={categories}
+          categoryName={categoryName}
           key={categoryName}
-          title={
-            <React.Fragment style="display: flex; justify-content: flex-end">
-              {categoryName}
-              <>
-                <EditOutlined />
-                <CloseOutlined style={{ color: '#FF0000' }} />
-              </>
-            </React.Fragment>
-          }
-          icon={<DownOutlined style={{ position: 'relative', top: -2 }} />}
-        >
-          {categories[categoryName].map((subCategory) => (
-            <Menu.Item key={subCategory} className="subcategory">
-              {subCategory}
-              <div>
-                <EditOutlined />
-                <CloseOutlined style={{ color: '#FF0000' }} />
-              </div>
-            </Menu.Item>
-          ))}
-          <Menu.Item key="Add Subcategory" className="subcategory">
-            Add Subcategory
-            <PlusOutlined style={{ position: 'relative', top: 10 }} />
-          </Menu.Item>
-        </SubMenu>
+        />
       ))}
       <Menu.Item key="Add Category" className="subcategory">
         Add Category
