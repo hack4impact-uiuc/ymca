bc127030c5d62289303e639ed7ff0a9425943903
Jacqueline <jackieo5023@gmail.com>
2021-03-18 22:56:44 -0500

    Add FormatJS to resource detail page

--- client/src/utils/messages.js
+++ client/src/utils/messages.js
@@ -35,3 +35,86 @@ export const filterMessages = defineMessages({
     defaultMessage: 'Name',
   },
 });
+
+export const detailMessages = defineMessages({
+  noAddress: {
+    id: 'detailNoAddress',
+    defaultMessage: 'No address provided',
+  },
+  noWebsite: {
+    id: 'detailNoWebsite',
+    defaultMessage: 'No website provided',
+  },
+  noEmail: {
+    id: 'detailNoEmail',
+    defaultMessage: 'No email provided',
+  },
+  noPhoneNumber: {
+    id: 'detailNoPhoneNumber',
+    defaultMessage: 'No phone number provided',
+  },
+  noDescription: {
+    id: 'detailNoDescription',
+    defaultMessage: 'No description provided',
+  },
+  eligibility: {
+    id: 'detailEligibility',
+    defaultMessage: 'Eligibility Requirements',
+  },
+  basicInfo: {
+    id: 'detailBasicInfo',
+    defaultMessage: 'Basic Information',
+  },
+  requiredDoc: {
+    id: 'detailRequiredDoc',
+    defaultMessage: 'Required Documents',
+  },
+  noneProvided: {
+    id: 'detailNoneProvided',
+    defaultMessage: 'None provided',
+  },
+  languagesSpoken: {
+    id: 'detailLanguagesSpoken',
+    defaultMessage: 'Languages Spoken',
+  },
+  financialAid: {
+    id: 'detailFinancialAid',
+    defaultMessage: 'Financial Aid',
+  },
+  education: {
+    id: 'detailEducation',
+    defaultMessage: 'Education',
+  },
+  immigrationStatus: {
+    id: 'detailImmigrationStatus',
+    defaultMessage: 'Immigration Status',
+  },
+  deadline: {
+    id: 'detailDeadline',
+    defaultMessage: 'Deadline',
+  },
+  amount: {
+    id: 'detailAmount',
+    defaultMessage: 'Amount',
+  },
+  locationAndHours: {
+    id: 'detailLocationAndHours',
+    defaultMessage: 'Location and Hours',
+  },
+  openNow: {
+    id: 'detailOpenNow',
+    defaultMessage: 'Open now!',
+  },
+  noSchedule: {
+    id: 'detailNoSchedule',
+    defaultMessage: 'No schedule provided',
+  },
+  none: {
+    id: 'detailNone',
+    defaultMessage: 'None',
+  },
+  schedule: {
+    id: 'detailSchedule',
+    defaultMessage: 'Schedule',
+  },
+});
