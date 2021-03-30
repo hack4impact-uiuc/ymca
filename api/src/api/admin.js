baf26a4c0588e38d8e1c0ad9bb85e096b6ced955
Michael Chen <36244017+Feeshay@users.noreply.github.com>
2021-03-29 19:38:31 -0500

    🖊️ Add endpoint to edit resource categories (#395)
    * tested and it works
    
    * frontend api
    
    Co-authored-by: Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>

--- api/src/api/admin.js
+++ api/src/api/admin.js
@@ -146,6 +146,35 @@ router.post(
   }),
 );
 
+// Edit categories of a resource
+router.patch(
+  '/resources/:id',
+  errorWrap(async (req, res) => {
+    if (req.body.category && req.body.subcategory) {
+      const { id } = req.params;
+      const updatedResource = await Resource.findByIdAndUpdate(
+        id,
+        {
+          $set: {
+            category: req.body.category,
+            subcategory: req.body.subcategory,
+          },
+        },
+        {
+          new: true,
+          runValidators: true,
+        },
+      );
+      res.json({
+        code: 200,
+        message: `Successfully updated resource ${id}`,
+        success: true,
+        result: updatedResource,
+      });
+    }
+  }),
+);
+
 // Edit an existing resource
 router.put(
   '/resources/:id',
