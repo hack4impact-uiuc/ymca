35f8ee04ecdd074660052d91877c47497fff48b1
Jacqueline Osborn <jackieo5023@gmail.com>
2021-04-01 19:36:22 -0500

    ♥️ Use FormatJS on the rest of the site (#401)

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,53 +2,106 @@
 
 import React from 'react';
 import { Layout, Tooltip } from 'antd';
+import { useIntl, FormattedMessage, defineMessages } from 'react-intl';
 
 import '../css/Footer.css';
 import useWindowDimensions from '../utils/mobile';
 
 const AntFooter = Layout.Footer;
 
+const messages = defineMessages({
+  builtWith: {
+    id: 'builtWith',
+    defaultMessage: 'Built with {heart} by {linkH4I}',
+  },
+  usageGuide: {
+    id: 'usageGuide',
+    defaultMessage: 'Usage Guide',
+  },
+  comingSoon: {
+    id: 'comingSoon',
+    defaultMessage: 'Coming soon!',
+  },
+  feedback: {
+    id: 'feedback',
+    defaultMessage: 'Feedback',
+  },
+});
+
 const Footer = () => {
   const isMobile = useWindowDimensions()[1];
   return isMobile ? <FooterMobile /> : <FooterDesktop />;
 };
 
-const FooterDesktop = () => (
-  <AntFooter className="footer">
-    <div className="footer-left">
-      Built with{' '}
-      <span role="img" aria-label="heart">
-        &#x1F49C;
-      </span>{' '}
-      by <a href="https://uiuc.hack4impact.org/">Hack4Impact UIUC</a> &copy;
-      2020
-    </div>
-    <div className="footer-list">
-      <Tooltip title="Coming soon!">
-        <span>Usage Guide</span>
-      </Tooltip>{' '}
-      &bull; <a href="https://forms.gle/iELSfzN1EAfVGYot5">Feedback</a>
-    </div>
-  </AntFooter>
-);
+const FooterDesktop = () => {
+  const intl = useIntl();
 
-const FooterMobile = () => (
-  <AntFooter className="footer-mobile">
-    <div className="footer-mobile-item">
-      <Tooltip title="Coming soon!">
-        <span>Usage Guide</span>
-      </Tooltip>{' '}
-      &bull; <a href="https://forms.gle/iELSfzN1EAfVGYot5">Feedback</a>
-    </div>
-    <>
-      Built with{' '}
-      <span role="img" aria-label="heart">
-        &#x1F49C;
-      </span>{' '}
-      by <a href="https://uiuc.hack4impact.org/">Hack4Impact UIUC</a> &copy;
-      2020
-    </>
-  </AntFooter>
-);
+  return (
+    <AntFooter className="footer">
+      <div className="footer-left">
+        <FormattedMessage
+          {...messages.builtWith}
+          values={{
+            heart: (
+              <span role="img" aria-label="heart">
+                &#x1F49C;
+              </span>
+            ),
+            linkH4I: (
+              <a href="https://uiuc.hack4impact.org/">Hack4Impact UIUC</a>
+            ),
+          }}
+        />{' '}
+        &copy; 2020
+      </div>
+      <div className="footer-list">
+        <Tooltip title={intl.formatMessage(messages.comingSoon)}>
+          <span>
+            <FormattedMessage {...messages.usageGuide} />
+          </span>
+        </Tooltip>{' '}
+        &bull;{' '}
+        <a href="https://forms.gle/iELSfzN1EAfVGYot5">
+          <FormattedMessage {...messages.feedback} />
+        </a>
+      </div>
+    </AntFooter>
+  );
+};
+
+const FooterMobile = () => {
+  const intl = useIntl();
+  return (
+    <AntFooter className="footer-mobile">
+      <div className="footer-mobile-item">
+        <Tooltip title={intl.formatMessage(messages.comingSoon)}>
+          <span>
+            <FormattedMessage {...messages.usageGuide} />
+          </span>
+        </Tooltip>{' '}
+        &bull;{' '}
+        <a href="https://forms.gle/iELSfzN1EAfVGYot5">
+          <FormattedMessage {...messages.feedback} />
+        </a>
+      </div>
+      <>
+        <FormattedMessage
+          {...messages.builtWith}
+          values={{
+            heart: (
+              <span role="img" aria-label="heart">
+                &#x1F49C;
+              </span>
+            ),
+            linkH4I: (
+              <a href="https://uiuc.hack4impact.org/">Hack4Impact UIUC</a>
+            ),
+          }}
+        />{' '}
+        &copy; 2020
+      </>
+    </AntFooter>
+  );
+};
 
 export default Footer;
