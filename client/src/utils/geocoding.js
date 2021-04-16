26e1384cd08e726ae87ff6479321b68a499396d2
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2021-04-15 20:03:59 -0500

    formatting again

--- client/src/utils/geocoding.js
+++ client/src/utils/geocoding.js
@@ -1,12 +1,12 @@
-const searchLocation = (input) => {
-  return fetch(
+const searchLocation = (input) =>
+  fetch(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${input}` +
       `.json?bbox=-92,37,-87,43&access_token=pk.` +
-      `eyJ1IjoiYW5vb2psYWwiLCJhIjoiY2syemtiYjZoMGp1eDNscXQ3azJzajl0bCJ9.FDSFjP1IfSisbm4uvd70vg`,
+      `eyJ1IjoiYW5vb2psYWwiLCJhIjoiY2syemtiYjZoMGp1eDNscXQ3azJzajl0bCJ9` +
+      `.FDSFjP1IfSisbm4uvd70vg`,
     {
       method: 'GET',
     },
   ).then((res) => res.json());
-};
 
 export default searchLocation;
