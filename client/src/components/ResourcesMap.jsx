e520df5c7ca446fc6f2b773ce361d636d73c7ece
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2021-04-19 19:22:03 -0500

    search bar works

--- client/src/components/ResourcesMap.jsx
+++ client/src/components/ResourcesMap.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-import { Input } from 'antd';
+import React, { useState } from 'react';
+import { AutoComplete } from 'antd';
+import { HomeTwoTone } from '@ant-design/icons';
+import searchLocation from '../utils/geocoding';
 
 function ResourcesMap() {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [autofillOptions, setAutofillOptions] = useState([]);
+
+  const onLocationSearch = async (search) => {
+    setSearchValue(search);
+    if (search && search.length >= 2) {
+      const results = await searchLocation(search);
+      setSearchResults(results.features);
+      setAutofillOptions(
+        results.features.map((place, idx) => {
+          const split = place.place_name.split(', ');
+          split.pop();
+          return { value: split.join(', ') };
+        }),
+      );
+    } else {
+      setSearchResults([]);
+    }
+  };
+
   return (
     <div>
       Map
-      <Input />
+      <br />
+      <HomeTwoTone style={{ fontSize: '20px', marginRight: '5px' }} />
+      <AutoComplete
+        allowClear
+        onChange={onLocationSearch}
+        options={autofillOptions}
+        placeholder="Enter your location here..."
+        style={{ width: '200px' }}
+      />
     </div>
   );
 }
--- client/src/utils/geocoding.js
+++ client/src/utils/geocoding.js
@@ -1,7 +1,7 @@
 const searchLocation = (input) =>
   fetch(
     `https://api.mapbox.com/geocoding/v5/mapbox.places/${input}` +
-      `.json?bbox=-92,37,-87,43&access_token=pk.` +
+      `.json?bbox=-88.5,39.9,-88,40.4&access_token=pk.` +
       `eyJ1IjoiYW5vb2psYWwiLCJhIjoiY2syemtiYjZoMGp1eDNscXQ3azJzajl0bCJ9` +
       `.FDSFjP1IfSisbm4uvd70vg`,
     {
