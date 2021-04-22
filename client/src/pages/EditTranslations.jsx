d0639766a979280e398c136c717ef3c1f676ce9d
Luciana <55062455+lucianatl@users.noreply.github.com>
2021-04-22 18:57:05 -0500

    🦕 Edit translations page (#426)
    * adding new page edittranslaitons
    
    * adding private route
    
    * fixed grids and css for edit translations page
    
    * stash
    
    * hopefully fixed foot and spacing
    
    * looking nice
    
    * fixing spelling
    
    Co-authored-by: Jacqueline Osborn <jackieo5023@gmail.com>

---
+++ client/src/pages/EditTranslations.jsx
@@ -0,0 +1,64 @@
+// @flow
+
+import React from 'react';
+import { Row, Progress, Checkbox, Layout, Button, Input } from 'antd';
+
+import '../css/EditTranslations.css';
+
+const { Header } = Layout;
+
+function onChange(e) {
+  console.log(`checked = ${e.target.checked}`);
+}
+
+function Translations() {
+  return (
+    <div className="edit-tranlsations">
+      <Header className="header">
+        <Row justify="left" type="flex">
+          <h2>Edit Resource Translations</h2>
+        </Row>
+      </Header>
+
+      <div>
+        <div className="title">
+          <div> </div>
+          <div>English</div>
+          <div className="to-translate-language">Español</div>
+          <div>Verified?</div>
+        </div>
+        <div className="translation-container">
+          <div className="resource-description">Resource Descriptions</div>
+          <div className="text-to-translate">
+            Assist with insurance applications, community care applications, and
+            general orientation of the health care system.
+          </div>
+          <div className="translation-input">
+            <Input placeholder="Translation" />
+          </div>
+          <Checkbox onChange={onChange} className="checkbox-translation">
+            {' '}
+          </Checkbox>
+        </div>
+        <div className="grid">
+          <div>
+            <div className="progress-bar-text">0/11 translations verified</div>
+            <Progress
+              percent={30}
+              strokeColor="purple"
+              className="progress-bar"
+              showInfo={false}
+            />
+          </div>
+          <div className="submit-button-wrapper">
+            <Button type="primary" className="submit-button">
+              Submit
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Translations;
