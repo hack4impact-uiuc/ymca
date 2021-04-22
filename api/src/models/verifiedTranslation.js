0075965093bab0170337903fc2e09fdaf3cfc99b
Jacqueline <jackieo5023@gmail.com>
2021-04-22 17:46:36 -0500

    Remove comments

--- api/src/models/verifiedTranslation.js
+++ api/src/models/verifiedTranslation.js
@@ -13,15 +13,3 @@ const VerifiedTranslation = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('VerifiedTranslation', VerifiedTranslation);
-
-// resource id
-// collection of objects:
-// { documentID (resource id, category id, subcategory id, testimonial id), type,
-// translation id, verified, num reports }
-
-// backend for form needs to return all text for a resource, along with its' translations, along with its' verified or not
-// { translation id, verified, num reports, english text, translated text }
-
-// endpoint for table returns name, priority, status, type,
-// resource/category/subcategory/testimonial-ID
-// TODO: jackie delete these comments when done with backend
