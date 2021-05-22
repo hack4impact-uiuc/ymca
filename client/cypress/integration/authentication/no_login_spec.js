991e00e92e2095b55b9c49fee72977ca9cd4b8e6
n3a9 <aggarwal.neeraj141@gmail.com>
2021-05-22 16:14:10 -0700

    Add more tests

--- client/cypress/integration/authentication/no_login_spec.js
+++ client/cypress/integration/authentication/no_login_spec.js
@@ -2,12 +2,12 @@
 
 context('No Login', () => {
   it('no login /saved redirects to /resources', () => {
-    cy.visit('/saved', {timeout: 10000});
+    cy.visit('/saved', { timeout: 10000 });
     cy.url().should('match', /resources/);
   });
 
   it('no login /translations redirects to /login', () => {
-    cy.visit('/translations', {timeout: 10000});
+    cy.visit('/translations', { timeout: 10000 });
     cy.url().should('match', /login/);
   });
 });
