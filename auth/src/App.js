2d0e2d2bfcb30cb23ca266519f151ea63cfe8188
n3a9 <neerajaggarwal@fb.com>
2021-05-21 17:23:31 -0700

    Add express rate limit for auth, update codeql workflow

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,19 +2,26 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const RateLimit = require("express-rate-limit");
 const { sendResponse } = require("./utils/sendResponse");
 const router = require("./api/index");
 
 const app = express();
 require("dotenv").config();
 
+const limiter = new RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 5,
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(limiter);
 
 app.use("/", router);
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.error(err);
   console.log(err.stack);
   sendResponse(
